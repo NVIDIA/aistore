2ed0710a95a95dfc5ad62414a2265908731d5cef
sasanap <sachin.sanap.r@gmail.com>
2018-04-11 16:24:45 -0400

    Fixed few issues after trying out cluster build out in AWS

---
+++ ansible/createdfcvolumes.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+set -e
+export AWS_ACCESS_KEY_ID=AKIAJMVS4P7LW7GZ4LHA
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_DEFAULT_REGION=us-east-2
+
+declare -a devices=(l m n o p q r s t u v w x y z)
+for i in `cat inventory/targets.txt`; do
+	instanceId=$(aws ec2 describe-instances --filters "Name=private-ip-address,Values='$i'" | grep InstanceId | cut -d\" -f4)
+	echo Target IP $i the instanceId is $instanceId
+	for j in $(seq 1 $1); do 
+		volumeId=$(aws ec2 create-volume --availability-zone us-east-2b --volume-type standard --size 1000 | grep VolumeId | cut -d\" -f4)
+		echo New volume created $volumeId
+		device=/dev/xvd${devices[$((j-1))]}
+		echo aws ec2 attach-volume --volume-id ${volumeId} --instance-id ${instanceId} --device ${device} | tee -a attachvolumes.sh
+	done	
+done
+
+chmod 777 attachvolumes.sh
+
+echo Created attachvolumes.sh file in current dir, run it when volumes are ready to attach
+
