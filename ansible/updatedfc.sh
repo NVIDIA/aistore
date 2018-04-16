6389aa4d637bb36524e82a5b0ae396ea622af59e
sasanap <sachin.sanap.r@gmail.com>
2018-04-16 16:07:15 -0400

    remove copying mountdfc.sh in updatedfc.sh

--- ansible/updatedfc.sh
+++ ansible/updatedfc.sh
@@ -3,8 +3,6 @@ set -o xtrace
 set -e
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory/cluster.ini getdfc.yml
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory/cluster.ini configdfc.yml
-ansible targets -m copy -a "src=mountdfc.sh dest=/home/ubuntu/mountdfc.sh" -i inventory/cluster.ini --become
-ansible targets -m file -a "dest=/home/ubuntu/mountdfc.sh mode=777 owner=ubuntu group=ubuntu" -i inventory/cluster.ini --become
 ansible targets -m shell -a "/home/ubuntu/mountdfc.sh > mountdfc.log" -i inventory/cluster.ini --become
 ansible targets -m shell -a "mount | grep dfc" -i inventory/cluster.ini --become
 parallel-ssh -h inventory/proxy.txt -i 'nohup /home/ubuntu/startproxy.sh >/dev/null 2>&1'
