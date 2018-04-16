b0ad6a19f09a5f5623a1d9026176eac1acea1db6
sasanap <sachin.sanap.r@gmail.com>
2018-04-16 19:25:03 -0400

    Add 5 seconds sleep before starting dfc daemons

--- ansible/startdfc.sh
+++ ansible/startdfc.sh
@@ -1,7 +1,9 @@
 echo Starting primary proxy
 parallel-ssh -h inventory/proxy.txt -i 'nohup /home/ubuntu/startprimaryproxy.sh >/dev/null 2>&1'
+sleep 5
 echo Starting stub proxies on every target node
 parallel-ssh -h inventory/targets.txt -i 'nohup /home/ubuntu/startproxy.sh >/dev/null 2>&1'
+sleep 5
 echo Starting all targets
 parallel-ssh -h inventory/targets.txt -i 'nohup /home/ubuntu/starttarget.sh >/dev/null 2>&1'
 echo Wait 10 seconds before checking DFC health
