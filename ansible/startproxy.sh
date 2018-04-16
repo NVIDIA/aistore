fd43f43e9aba46e10511f563f2ab1d9da5d36bfe
sasanap <sachin.sanap.r@gmail.com>
2018-04-16 14:32:28 -0400

    Added full path to dfc executable in startporxy.sh

--- ansible/startproxy.sh
+++ ansible/startproxy.sh
@@ -1,3 +1,3 @@
 ps -C dfc -o pid= | xargs sudo kill -9
-sudo $GOBIN/dfc -config=/home/ubuntu/dfc.json -role=proxy -ntargets=6 &
+sudo /home/ubuntu/dfc/bin/dfc -config=/home/ubuntu/dfc.json -role=proxy -ntargets=6 &
 
