767ea61dd9fe49908cfd0108d33ce6a037604f0e
sasanap <sachin.sanap.r@gmail.com>
2018-04-16 19:16:51 -0400

    Change port for co-existing proxies with targets

--- ansible/startproxy.sh
+++ ansible/startproxy.sh
@@ -1,5 +1,5 @@
-ps -C dfc -o pid= | xargs sudo kill -9
 cp dfc.json dfcproxy.json
 sed -i '/logdir/c\"logdir": "/var/log/dfcproxy"' dfcproxy.json
+sed -i '/"port":/c\"port": "8082"' dfcproxy.json
 sudo /home/ubuntu/dfc/bin/dfc -config=/home/ubuntu/dfcproxy.json -role=proxy -ntargets=6 &
 
