27073e98bdd38e9dffa862aa0893e024a3155583
sasanap <sachin.sanap.r@gmail.com>
2018-04-17 12:14:58 -0400

    Primary proxy log location changed to /var/log/dfcproxy to match with stub proxies

--- ansible/startprimaryproxy.sh
+++ ansible/startprimaryproxy.sh
@@ -1,4 +1,5 @@
-ps -C dfc -o pid= | xargs sudo kill -9
+cp dfc.json dfcproxy.json
+sed -i '/logdir/c\"logdir": "/var/log/dfcproxy",' dfcproxy.json
 export DFCPRIMARYPROXY=True
 sudo -E /home/ubuntu/dfc/bin/dfc -config=/home/ubuntu/dfc.json -role=proxy -ntargets=6 &
 
