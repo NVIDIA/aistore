aa7bdaf65b35ecbdeda19f2a91f6dfde71c604c8
sasanap <sachin.sanap.r@gmail.com>
2018-04-18 18:57:06 -0400

    cat INFO and ERROR log on all nodes after starting cluster

--- ansible/startdfc.sh
+++ ansible/startdfc.sh
@@ -6,10 +6,16 @@ parallel-ssh -h inventory/targets.txt -i 'nohup /home/ubuntu/startproxy.sh >/dev
 sleep 5
 echo Starting all targets
 parallel-ssh -h inventory/targets.txt -i 'nohup /home/ubuntu/starttarget.sh >/dev/null 2>&1'
-echo Wait 10 seconds before checking DFC health
-sleep 10
+echo Wait 20 seconds before checking DFC health
+sleep 20
 parallel-ssh -h inventory/proxy.txt -i 'ps -C dfc'
 parallel-ssh -h inventory/targets.txt -i 'ps -C dfc'
 parallel-ssh -h inventory/proxy.txt -i 'cat ~/dfc.json'
-parallel-ssh -h inventory/proxy.txt -i 'tail -20 /var/log/dfc/dfc.INFO'
+parallel-ssh -h inventory/proxy.txt -i 'tail -20 /var/log/dfcproxy/dfc.INFO'
+parallel-ssh -h inventory/targets.txt -i 'tail -20 /var/log/dfc/dfc.INFO'
+parallel-ssh -h inventory/targets.txt -i 'tail -20 /var/log/dfcproxy/dfc.INFO'
+parallel-ssh -h inventory/proxy.txt -i 'tail -20 /var/log/dfcproxy/dfc.ERROR' || true
+parallel-ssh -h inventory/targets.txt -i 'tail -20 /var/log/dfc/dfc.ERROR' || true
+parallel-ssh -h inventory/targets.txt -i 'tail -20 /var/log/dfcproxy/dfc.ERROR' || true
+
 
