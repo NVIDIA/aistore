2241a857875deca1b1a71b9085d8913dc1d30e38
sasanap <sachin.sanap.r@gmail.com>
2018-04-19 13:16:05 -0400

    Fix starprimaryproxy to pick right json

--- ansible/startprimaryproxy.sh
+++ ansible/startprimaryproxy.sh
@@ -1,5 +1,5 @@
 cp dfc.json dfcproxy.json
 sed -i '/logdir/c\"logdir": "/var/log/dfcproxy",' dfcproxy.json
 export DFCPRIMARYPROXY=True
-sudo -E /home/ubuntu/dfc/bin/dfc -config=/home/ubuntu/dfc.json -role=proxy -ntargets=6 &
+sudo -E /home/ubuntu/dfc/bin/dfc -config=/home/ubuntu/dfcproxy.json -role=proxy -ntargets=6 &
 
--- ansible/stopandcleandfc.sh
+++ ansible/stopandcleandfc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-parallel-ssh -h inventory/targets.txt -i 'ps -C dfc -o pid= | xargs sudo kill'
-parallel-ssh -h inventory/targets.txt -i 'ps -C dfc -o pid= | xargs sudo kill'
+parallel-ssh -h inventory/targets.txt -i 'ps -C dfc -o pid= | xargs sudo kill || true'
+parallel-ssh -h inventory/targets.txt -i 'ps -C dfc -o pid= | xargs sudo kill || true'
 parallel-ssh -h inventory/proxy.txt -i 'ps -C dfc -o pid= | xargs sudo kill'
 parallel-ssh -h inventory/proxy.txt -i 'sudo rm -rf /var/log/dfc*'
 parallel-ssh -h inventory/targets.txt -i 'sudo rm -rf /var/log/dfc*'
