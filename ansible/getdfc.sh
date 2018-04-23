a65ef51bbdfa4cf9af24d31274bb58accf676279
sasanap <sachin.sanap.r@gmail.com>
2018-04-23 13:52:56 -0400

    Seperate new node setup and already setup but rebooted node setup

--- ansible/getdfc.sh
+++ ansible/getdfc.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 . /etc/profile.d/dfcpaths.sh
-echo 'Go get DFC'
-rm -rf ~/dfc || true
-mkdir -p ~/dfc/{bin,pkg,src}
-/usr/local/go/bin/go get -u -v github.com/NVIDIA/dfcpub/dfc
+echo 'Pull latest DFC'
 cd $DFCSRC
+git pull
 BUILD=`git rev-parse --short HEAD`
 /usr/local/go/bin/go build && go install && GOBIN=$GOPATH/bin go install -ldflags "-X github.com/NVIDIA/dfcpub/dfc.build=$BUILD" setup/dfc.go
-
--- ansible/updatedfc.sh
+++ ansible/updatedfc.sh
@@ -2,7 +2,7 @@
 set -o xtrace
 set -e
 ./stopandcleandfc.sh
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory/cluster.ini setupnodes.yml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory/cluster.ini copyscripts.yml
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory/cluster.ini getdfc.yml
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory/cluster.ini configdfc.yml
 ansible targets -m shell -a "/home/ubuntu/mountdfc.sh > mountdfc.log" -i inventory/cluster.ini --become
