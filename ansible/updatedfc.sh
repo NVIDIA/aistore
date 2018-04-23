f9810761f8627e64b651f746fd403c98ece19539
sasanap <sachin.sanap.r@gmail.com>
2018-04-23 14:12:17 -0400

    Fix enablestats.sh execution

--- ansible/updatedfc.sh
+++ ansible/updatedfc.sh
@@ -7,6 +7,8 @@ ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory/cluster.ini getdfc
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory/cluster.ini configdfc.yml
 ansible targets -m shell -a "/home/ubuntu/mountdfc.sh > mountdfc.log" -i inventory/cluster.ini --become
 ansible targets -m shell -a "mount | grep dfc" -i inventory/cluster.ini --become
-./enablestats.sh || true
+parallel-ssh -h inventory/cluster.txt -i 'nohup ./enablestats.sh >/dev/null 2>&1' || true
+parallel-ssh -h inventory/cluster.txt -i 'ps -leaf | grep statsd' || true
+parallel-ssh -h inventory/cluster.txt -i 'service collectd status' || true
 ./startdfc.sh
 
