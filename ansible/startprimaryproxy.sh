bd621e19675d65bff92e3ce7878bcd1674bd1488
sasanap <sachin.sanap.r@gmail.com>
2018-04-26 15:26:58 -0400

    start proxy add sudo in cp command

--- ansible/startprimaryproxy.sh
+++ ansible/startprimaryproxy.sh
@@ -1,4 +1,4 @@
-cp dfc.json dfcproxy.json
+sudo cp dfc.json dfcproxy.json
 sed -i '/logdir/c\"logdir": "/var/log/dfcproxy",' dfcproxy.json
 export DFCPRIMARYPROXY=True
 sudo -E /home/ubuntu/dfc/bin/dfc -config=/home/ubuntu/dfcproxy.json -role=proxy -ntargets=6 &
--- ansible/startproxy.sh
+++ ansible/startproxy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-cp dfc.json dfcproxy.json
+sudo cp dfc.json dfcproxy.json
 sed -i '/logdir/c\"logdir": "/var/log/dfcproxy",' dfcproxy.json
 sed -i '/"port":/c\"port": "8082"' dfcproxy.json
 sudo /home/ubuntu/dfc/bin/dfc -config=/home/ubuntu/dfcproxy.json -role=proxy -ntargets=6 &
