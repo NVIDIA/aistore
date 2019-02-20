aba54e943b11e8b4101c4b33d9b2da715af0c19b
Soumya Balakrishnan <soumyab@nvidia.com>
2019-02-20 14:30:26 -0800

    AIS helm chart script - Fix some config templates to use common_config object

--- deploy/prod/kubernetes/ais_helm/charts/templates/_set_initial_primary_proxy_env.sh
+++ deploy/prod/kubernetes/ais_helm/charts/templates/_set_initial_primary_proxy_env.sh
@@ -1,7 +1,7 @@
 {{- define "proxy.set_initial_primary_proxy_env" -}}
 #!/bin/bash
 # MY_NODE is a environment with downward api to get the node of a pod
-if  kubectl get nodes --show-labels | grep {{ .Values.proxy.initialPrimaryProxyNodeLabel.name | grep -w $MY_NODE }} ; then
+if  kubectl get nodes --show-labels | grep {{ .Values.proxy.initialPrimaryProxyNodeLabel.name }} | grep -w $MY_NODE ; then
     echo "This is the node ($MY_NODE) with label {{ .Values.proxy.initialPrimaryProxyNodeLabel.name }}. Setup environment"
     echo "export AIS_PRIMARYPROXY=True" > {{ .Values.proxy.envMountPath.podPath }}/env
     cat {{ .Values.proxy.envMountPath.podPath }}/env
