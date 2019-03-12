021c82e32a0629fa6c766b7508a93da327641bb3
soumyab <soumyab@nvidia.com>
2019-03-12 16:47:06 -0700

    Fix any automation bugs
    Fail the script if docker_token is not specified
    
    fix comments

--- deploy/prod/kubernetes/helm_charts/ais/setup_ais_configfile.sh
+++ deploy/prod/kubernetes/helm_charts/ais/setup_ais_configfile.sh
@@ -10,8 +10,10 @@ bash ${AISSRC}/setup/config.sh
 sed -i '1s/^/{{- define "target.config" -}}/' _ais_target.json
 sed -i '1s/^/{{- define "proxy.config" -}}/' _ais_proxy.json
 sed -i '1s/^/{{- define "ne_proxy.config" -}}/' _ais_ne_proxy.json
-sed -i '1s/^/{{- define "collectd.config" -}}/' _collectd.json
-sed -i '1s/^/{{- define "statsd.config" -}}/' _statsd.json
+
+#statsd and collectd templates now take values from the newly created graphite charts
+#so remove them from the current directory so they do not replace the helm chart templates
+rm _statsd.json _collectd.json
 
 files=$(find . -type f -name "_*.json" -maxdepth 1)
 for i in $files; do echo "{{- end -}}" >> $i; done
--- deploy/prod/kubernetes/helm_install_scripts/deploy_helm.sh
+++ deploy/prod/kubernetes/helm_install_scripts/deploy_helm.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+usage() {
+    echo
+    echo "Usage: $0 kube_conf app_name docker_repo_token";
+    echo " where :"
+    echo "  kube_conf         : path to kube-config to the K8S cluster"
+    echo "  app_name          : helm application name to deploy"
+    echo "  docker_repo_token : auth token to pull the docker image to deploy the helm chart"
+    echo
+    exit -1
+}
+
+if [ $# != 3 ]; then
+    usage
+fi
 
 export KUBECONFIG=$1
 APP_NAME=$2
+DOCKER_REPO_TOKEN=$3
 
-//TODO: ADD CHECKS TO ENSURE THE TWO REQUIRED PARAMETERS ARE SPECIFIED
+//TO DO: ADD CHECKS TO ENSURE THE TWO REQUIRED PARAMETERS ARE SPECIFIED
 
 set -e
 
@@ -16,6 +31,13 @@ if [[ -f image_tag.txt ]]; then
     image_tag=`cat image_tag.txt`
     helm_args=" --set image.tag=${image_tag} "
     echo "helm install with ARGS: ${helm_args}"
+fi
+
+if [[ ! -z $DOCKER_REPO_TOKEN ]];then
+  helm_args+=" -- set image.dockerRepoToken=\"$DOCKER_REPO_TOKEN\""
+else
+  echo "ERROR: DOCKER_REPO_TOKEN needs to be passed" >&2
+  usage;
 fi
 
 # Check if the app has been installed
@@ -32,6 +54,7 @@ fi
 #echo "${APP_NAME} does not exist. Installing..."
 
     echo "Installing ${APP_NAME}..."
+    helm dependency update
     helm install --name=${APP_NAME} ${helm_args}  .
 #fi
 
