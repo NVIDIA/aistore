9bd9f9d04f6769f974af478e50a918c0192e3bb4
Vladimir Markelov <vmarkelov@nvidia.com>
2019-03-15 11:26:24 -0700

    return correct errors for non-existing bucket
    If Cloud is disabled all request to invalid bucket must return HTTP
    error 404 with message "bucket not found"

--- ais/cloud_mock.go
+++ ais/cloud_mock.go
@@ -6,6 +6,8 @@ package ais
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/NVIDIA/aistore/cluster"
@@ -16,6 +18,8 @@ type (
 	emptyCloud struct{} // mock
 )
 
+const bucketDoesNotExist = "bucket %q %s"
+
 var (
 	_ cloudif = &emptyCloud{}
 )
@@ -23,23 +27,25 @@ var (
 func newEmptyCloud() *emptyCloud { return &emptyCloud{} }
 
 func (m *emptyCloud) listbucket(ctx context.Context, bucket string, msg *cmn.GetMsg) (jsbytes []byte, errstr string, errcode int) {
-	return []byte{}, "", 0
+	return []byte{}, fmt.Sprintf(bucketDoesNotExist, bucket, cmn.DoesNotExist), http.StatusNotFound
 }
 func (m *emptyCloud) headbucket(ctx context.Context, bucket string) (bucketprops cmn.SimpleKVs, errstr string, errcode int) {
-	return
-}
-func (m *emptyCloud) getbucketnames(ctx context.Context) (buckets []string, errstr string, errcode int) {
-	return []string{}, "", 0
+	return cmn.SimpleKVs{}, fmt.Sprintf(bucketDoesNotExist, bucket, cmn.DoesNotExist), http.StatusNotFound
 }
 func (m *emptyCloud) headobject(ctx context.Context, bucket string, objname string) (objmeta cmn.SimpleKVs, errstr string, errcode int) {
-	return cmn.SimpleKVs{}, "", 0
+	return cmn.SimpleKVs{}, fmt.Sprintf(bucketDoesNotExist, bucket, cmn.DoesNotExist), http.StatusNotFound
 }
 func (m *emptyCloud) getobj(ctx context.Context, fqn, bucket, objname string) (props *cluster.LOM, errstr string, errcode int) {
-	return
+	return nil, fmt.Sprintf(bucketDoesNotExist, bucket, cmn.DoesNotExist), http.StatusNotFound
 }
 func (m *emptyCloud) putobj(ctx context.Context, file *os.File, bucket, objname string, cksum cmn.CksumProvider) (version string, errstr string, errcode int) {
-	return
+	return "", fmt.Sprintf(bucketDoesNotExist, bucket, cmn.DoesNotExist), http.StatusNotFound
 }
 func (m *emptyCloud) deleteobj(ctx context.Context, bucket, objname string) (errstr string, errcode int) {
-	return
+	return fmt.Sprintf(bucketDoesNotExist, bucket, cmn.DoesNotExist), http.StatusNotFound
+}
+
+// the function must not fail - it should return empty list
+func (m *emptyCloud) getbucketnames(ctx context.Context) (buckets []string, errstr string, errcode int) {
+	return []string{}, "", 0
 }
