0fecfebc871bebfb1f40692362d76fb3ba057f5d
Alex Aizman <alex.aizman@gmail.com>
2020-02-19 18:01:27 -0800

    move/simplify dump/compress tools/tests

--- ais/dump/dump_md_test.go
+++ ais/dump/dump_md_test.go
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
  */
-package ais_test
+package dump_test
 
 import (
 	"flag"
@@ -20,47 +20,55 @@ import (
 )
 
 // Examples - Smap:
-// 1. go test -v ./ais/. -run=DumpSmap -fsmap=~/.ais0/.ais.smap
-// 2. go test -v ./ais/. -run=DumpSmap -fsmap=~/.ais0/.ais.smap -fout=/tmp/smap.txt
-// 3. go test -v ./ais/. -run=CompressSmap -fsmap=/tmp/smap.txt -fout=/tmp/.ais.smap
+// 1. go test -v -run=DumpSmap -in=~/.ais0/.ais.smap  # dump Smap to STDOUT
+// 2. go test -v -run=DumpSmap -in=~/.ais0/.ais.smap -out=/tmp/smap.txt
+// 3. go test -v -run=CompressSmap -in=/tmp/smap.txt -out=/tmp/.ais.smap
 //
 // Example - BMD:
-// 4. go test -v ./ais/. -run=DumpBMD -fbmd=~/.ais0/.ais.bmd -fout=/tmp/bmd.txt
-// 5. go test -v ./ais/. -run=CompressBMD -fbmd=/tmp/bmd.txt -fout=/tmp/.ais.bmd
+// 4. go test -v -run=DumpBMD -in=~/.ais0/.ais.bmd  # dump BMD to STDOUT
+// 5. go test -v -run=DumpBMD -in=~/.ais0/.ais.bmd -out=/tmp/bmd.txt
+// 6. go test -v -run=CompressBMD -in=/tmp/bmd.txt -out=/tmp/.ais.bmd
 
 var (
-	fsmap, fbmd, fout string
+	in, out string
 )
 
-func init() {
-	flag.StringVar(&fsmap, "fsmap", "", "fully-qualified Smap filename")
-	flag.StringVar(&fbmd, "fbmd", "", "fully-qualified BMD filename")
-	flag.StringVar(&fout, "fout", "", "output filename if defined, otherwise stdout")
+func TestMain(m *testing.M) {
+	var run bool
+	flag.StringVar(&in, "in", "", "fully-qualified filename")
+	flag.StringVar(&out, "out", "", "output filename (can be omitted when dumping BMD or Smap)")
+	flag.Parse()
+	for _, option := range os.Args {
+		if strings.Contains(option, "run") {
+			run = true
+		}
+	}
+	if in == "" && run {
+		fmt.Println("input (the -in option) is not defined")
+		os.Exit(1)
+	}
+	if run {
+		os.Exit(m.Run())
+	}
 }
 
 // "*Dump" routines expect compressed (bmd, smap)
 
 func TestDumpSmap(t *testing.T) {
-	if fsmap == "" {
-		t.Skip()
-	}
 	smap := &cluster.Smap{}
-	dumpMeta(t, fsmap, smap, jsp.Options{Signature: true})
+	dumpMeta(t, in, smap, jsp.Options{Signature: true})
 }
 
 func TestDumpBMD(t *testing.T) {
-	if fbmd == "" {
-		t.Skip()
-	}
 	bmd := &cluster.BMD{}
-	dumpMeta(t, fbmd, bmd, jsp.Options{Signature: true})
+	dumpMeta(t, in, bmd, jsp.Options{Signature: true})
 }
 
 func dumpMeta(t *testing.T, fn string, v interface{}, opts jsp.Options) {
 	var f = os.Stdout
 	var err error
-	if fout != "" {
-		f, err = cmn.CreateFile(_fclean(fout))
+	if out != "" {
+		f, err = cmn.CreateFile(_fclean(out))
 		tassert.CheckFatal(t, err)
 	}
 	err = jsp.Load(_fclean(fn), v, opts)
@@ -70,34 +78,28 @@ func dumpMeta(t *testing.T, fn string, v interface{}, opts jsp.Options) {
 	fmt.Fprintln(f, string(s))
 }
 
-// "*Compress" routines
+// "*Compress" routines require output filename
 
 func TestCompressSmap(t *testing.T) {
-	if fsmap == "" {
-		t.Skip()
-	}
-	if fout == "" {
-		t.Fatal("empty output filename")
+	if out == "" {
+		t.Fatal("output filename (the -out option) must be defined")
 	}
 	smap := &cluster.Smap{}
-	compressMeta(t, fsmap, smap, jsp.CCSign())
+	compressMeta(t, in, smap, jsp.CCSign())
 }
 
 func TestCompressBMD(t *testing.T) {
-	if fbmd == "" {
-		t.Skip()
-	}
-	if fout == "" {
-		t.Fatal("empty output filename")
+	if out == "" {
+		t.Fatal("output filename (the -out option) must be defined")
 	}
 	bmd := &cluster.BMD{}
-	compressMeta(t, fbmd, bmd, jsp.CCSign())
+	compressMeta(t, in, bmd, jsp.CCSign())
 }
 
 func compressMeta(t *testing.T, fn string, v interface{}, opts jsp.Options) {
 	err := jsp.Load(_fclean(fn), v, jsp.Plain())
 	tassert.CheckFatal(t, err)
-	err = jsp.Save(_fclean(fout), v, opts)
+	err = jsp.Save(_fclean(out), v, opts)
 	tassert.CheckFatal(t, err)
 }
 
