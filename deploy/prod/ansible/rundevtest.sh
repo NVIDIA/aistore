86c2d4f612a945fc478b30ab29fedb0f2e6cb013
Janusz Marcinkiewicz <januszm@nvidia.com>
2020-02-28 15:44:12 +0100

    build: minor refactor of Jenkins test script

--- deploy/prod/ansible/rundevtest.sh
+++ deploy/prod/ansible/rundevtest.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+
 set -o xtrace
 source /etc/profile.d/aispaths.sh
 source aws.env
-for pid in `ps -C aisnode -o pid=`; do echo Stopping AIStore $pid; sudo kill $pid; done
+
+for pid in $(ps -C aisnode -o pid=); do echo "stopping aisnode: $pid"; sudo kill $pid; done
 sudo rm -rf /home/ubuntu/.ais*
 rm -rf /tmp/ais*
-cd $AISSRC
-if [ ! -z $1 ]; then
-    echo Git checkout branch $1
+cd $AISTORE_SRC || exit 1
+
+if [[ -n $1 ]]; then
+    echo "git checkout branch: $1"
     git checkout $1
 fi
 
@@ -16,35 +19,30 @@ git reset --hard origin/master
 git status
 git log | head -5
 
-setup/deploy.sh -loglevel=3 -stats_time=10s <<< $'4\n4\n4\n1'
+make deploy <<< $'4\n4\n4\n1'
 
-echo sleep 10 seconds before checking AIStore process
+echo "sleep 10 seconds before checking AIStore processes"
 sleep 10
-aisprocs=$(ps -C aisnode -o pid= | wc -l)
-echo number of aisprocs $aisprocs
-if [ $aisprocs -lt 8 ]; then
-    echo aisnode did not start properly
+
+nodes=$(ps -C aisnode -o pid= | wc -l)
+echo "number of started aisprocs: $nodes"
+if [[ $nodes -lt 8 ]]; then
+    echo "some of the aisnodes did not start properly"
     exit 1
 fi
 
-echo Working with AIStore build -
-grep -r Build /tmp/ais | head -1
+echo "working with build: $(git rev-parse --short HEAD)"
+echo "run tests with cloud bucket: devtestcloud"
 
-cd ../
-cdir=$(pwd)
-echo Run go tests from $cdir
+BUCKET=devtestcloud make test-long
+exit_code=$?
 
-echo run AIStore tests with cloud bucket devtestcloud
+echo "'make test-long' exit status: $exit_code"
 
-BUCKET=devtestcloud go test -v -p 1 -count 1 -timeout 130m ./...
-
-cloudExitStatus=$?
-echo devtest exit status $cloudExitStatus
-
-for aispid in `ps -C aisnode -o pid=`; do echo Stopping AIStore $aispid; sudo kill $aispid; done
+for pid in $(ps -C aisnode -o pid=); do echo "stopping aisnode: $pid"; sudo kill $pid; done
 result=0
-if [ $cloudExitStatus -ne 0 ]; then
-    echo DevTests failed
+if [[ $exit_code -ne 0 ]]; then
+    echo "tests failed"
     result=$((result + 1))
 fi
 
