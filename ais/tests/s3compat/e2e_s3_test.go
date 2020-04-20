59f8e5425b717e0c11480db3fb64f8db196822c6
Vladimir Markelov <vmarkelov@nvidia.com>
2020-04-20 16:37:25 -0700

    S3Compat: add E2E tests for s3 compatibility layer(via aws cli)

---
+++ ais/tests/s3compat/e2e_s3_test.go
@@ -0,0 +1,45 @@
+package s3_integration
+
+import (
+	"os/exec"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/NVIDIA/aistore/tutils"
+	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/config"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+)
+
+func TestE2ES3(t *testing.T) {
+	cmd := exec.Command("which", "aws")
+	if err := cmd.Run(); err != nil {
+		t.Skip("'aws' binary not found")
+	}
+
+	config.DefaultReporterConfig.SlowSpecThreshold = 15 * time.Second.Seconds()
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "E2E")
+}
+
+var _ = Describe("E2E AWS Compatibility Tests", func() {
+	var (
+		entries []TableEntry
+
+		f        = &tutils.E2EFramework{}
+		files, _ = filepath.Glob("./*.in")
+	)
+
+	for _, fileName := range files {
+		fileName = fileName[:len(fileName)-len(filepath.Ext(fileName))]
+		entries = append(entries, Entry(fileName, fileName+".in", fileName+".stdout"))
+	}
+
+	DescribeTable(
+		"e2e",
+		f.RunE2ETest,
+		entries...,
+	)
+})
