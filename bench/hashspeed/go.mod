b4aaa6b866aff6d808b140ab4f3e2bbddfb0d04d
Alex Aizman <alex.aizman@gmail.com>
2020-06-03 09:44:29 -0700

    add xxhash vs highwayhash throughput benchmark

---
+++ bench/hashspeed/go.mod
@@ -0,0 +1,8 @@
+module github.com/NVIDIA/aistore/bench/hashspeed
+
+go 1.14
+
+require (
+	github.com/OneOfOne/xxhash v1.2.8
+	github.com/minio/highwayhash v1.0.0
+)
---
+++ bench/hashspeed/hash_test.go
@@ -0,0 +1,102 @@
+// Package hashspeed is a benchmark througput benchmark
+/*
+ * Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+ */
+package hashspeed
+
+import (
+	"hash"
+	"math/rand"
+	"runtime"
+	"sync/atomic"
+	"testing"
+
+	"github.com/OneOfOne/xxhash"
+	"github.com/minio/highwayhash"
+)
+
+// Examples:
+//
+// go test -bench=.
+// go test -bench=Through -benchtime=10s
+// go test -bench=Through -benchtime=30s -benchmem
+
+const maxSize = 64 * 1024 * 1024
+
+var (
+	cores = runtime.GOMAXPROCS(0)
+	hwkey [32]byte
+	vec   = make([][]byte, cores)
+)
+
+func BenchmarkThroughput(b *testing.B) {
+	rnd := rand.New(rand.NewSource(0xa5a5a5a5a5a5))
+	for i := range vec {
+		vec[i] = make([]byte, maxSize)
+		rnd.Read(vec[i])
+	}
+	tests := []struct {
+		name    string
+		size    int64
+		newHash func() (hash.Hash, error)
+	}{
+		{
+			name:    "highwayhash-1M",
+			size:    1024 * 1024,
+			newHash: func() (hash.Hash, error) { return highwayhash.New(hwkey[:]) },
+		},
+		{
+			name:    "highwayhash-8M",
+			size:    8 * 1024 * 1024,
+			newHash: func() (hash.Hash, error) { return highwayhash.New(hwkey[:]) },
+		},
+		{
+			name:    "highwayhash-64M",
+			size:    64 * 1024 * 1024,
+			newHash: func() (hash.Hash, error) { return highwayhash.New(hwkey[:]) },
+		},
+		{
+			name:    "xxhash-1M",
+			size:    1024 * 1024,
+			newHash: func() (hash.Hash, error) { return xxhash.New64(), nil },
+		},
+		{
+			name:    "xxhash-8M",
+			size:    8 * 1024 * 1024,
+			newHash: func() (hash.Hash, error) { return xxhash.New64(), nil },
+		},
+		{
+			name:    "xxhash-64M",
+			size:    64 * 1024 * 1024,
+			newHash: func() (hash.Hash, error) { return xxhash.New64(), nil },
+		},
+	}
+	for _, test := range tests {
+		b.Run(test.name, func(b *testing.B) {
+			throughput(b, test.size, test.newHash)
+		})
+	}
+}
+
+func throughput(b *testing.B, size int64, newHash func() (hash.Hash, error)) {
+	var iter uint64
+	b.SetBytes(size)
+	b.ResetTimer()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			h, err := newHash()
+			assert(err == nil, "new")
+			i := atomic.AddUint64(&iter, 1) % uint64(cores)
+			l, err := h.Write(vec[i][:size])
+			assert(int64(l) == size && err == nil, "write")
+			h.Sum(nil)
+		}
+	})
+}
+
+func assert(cond bool, msg string) {
+	if !cond {
+		panic(msg)
+	}
+}
