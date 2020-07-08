ef11a7c25f1474c3a5e1832f4af016772c27ee1a
Alex Aizman <alex.aizman@gmail.com>
2020-07-08 09:23:20 -0700

    fix log collection (J2)

--- deploy/prod/ansible/devtest.sh
+++ deploy/prod/ansible/devtest.sh
@@ -7,7 +7,7 @@ ssh $(head -1 inventory/targets.txt) './rundevtest.sh master'
 EXIT_STATUS=$?
 echo "RUNTEST exit status is ${EXIT_STATUS}"
 
-ssh $(head -1 inventory/targets.txt) 'sudo tar -czf --exclude="$trash" /tmp/devtest_$(hostname)_$(date +%Y%m%d-%H%M%S).tar.gz /tmp/ais /home/ubuntu/.ais* >/dev/null 2>&1'
+ssh $(head -1 inventory/targets.txt) 'sudo tar -czf /tmp/devtest_$(hostname)_$(date +%Y%m%d-%H%M%S).tar.gz /tmp/ais /home/ubuntu/.ais* >/dev/null'
 mkdir logs
 scp $(head -1 inventory/targets.txt):/tmp/*.tar.gz logs/
 echo "DevTest logs are copied here"
