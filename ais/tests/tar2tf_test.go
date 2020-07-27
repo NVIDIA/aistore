764930777e788deca5eabc3fefe4f25bacd2fc32
Tomasz Knopik <tknopik@nvidia.com>
2020-07-27 17:32:21 +0200

    transformations: add tar2tf range tests

--- ais/tests/tar2tf_test.go
+++ ais/tests/tar2tf_test.go
@@ -5,8 +5,10 @@
 package integration
 
 import (
-	"io"
+	"bytes"
+	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"path/filepath"
 	"testing"
@@ -20,6 +22,21 @@ import (
 	"github.com/NVIDIA/go-tfdata/tfdata/core"
 	jsoniter "github.com/json-iterator/go"
 )
+
+func startTar2TfTransformer(t *testing.T) (uuid string) {
+	transformerTemplate := filepath.Join("templates", "transformer", tar2tf, "pod.yaml")
+	spec, err := ioutil.ReadFile(transformerTemplate)
+	tassert.CheckError(t, err)
+
+	pod, err := transform.ParsePodSpec(spec)
+	tassert.CheckError(t, err)
+	spec, _ = jsoniter.Marshal(pod)
+
+	// Starting transformer
+	uuid, err = api.TransformInit(baseParams, spec)
+	tassert.CheckFatal(t, err)
+	return uuid
+}
 
 func TestKubeTar2TFS3(t *testing.T) {
 	tutils.CheckSkip(t, tutils.SkipTestArgs{Kubernetes: true})
@@ -56,29 +73,16 @@ func TestKubeTar2TFS3(t *testing.T) {
 	tassert.CheckFatal(t, api.PutObject(putArgs))
 	defer api.DeleteObject(baseParams, bck, tarObjName)
 
-	transformerTemplate := filepath.Join("templates", "transformer", tar2tf, "pod.yaml")
-	spec, err := ioutil.ReadFile(transformerTemplate)
-	tassert.CheckError(t, err)
-
-	pod, err := transform.ParsePodSpec(spec)
-	tassert.CheckError(t, err)
-	spec, _ = jsoniter.Marshal(pod)
-
-	// Starting transformer
-	uuid, err := api.TransformInit(baseParams, spec)
-	tassert.CheckFatal(t, err)
+	uuid := startTar2TfTransformer(t)
 	defer func() {
 		tassert.CheckFatal(t, api.TransformStop(baseParams, uuid))
 	}()
 
 	// GET TFRecord from TAR
-	outFileGet, err := ioutil.TempFile("", "tar2tf-")
-	tassert.CheckFatal(t, err)
+	outFileBuffer := bytes.NewBuffer(nil)
 
-	_, err = api.GetObjectS3(baseParams, bck, tarObjName+"!"+uuid, api.GetObjectInput{Writer: outFileGet})
+	_, err = api.GetObjectS3(baseParams, bck, tarObjName+"!"+uuid, api.GetObjectInput{Writer: outFileBuffer})
 	tassert.CheckFatal(t, err)
-	tassert.CheckFatal(t, outFileGet.Sync())
-	_, err = outFileGet.Seek(0, io.SeekStart)
 	tassert.CheckFatal(t, err)
 
 	// Comparing actual vs expected
@@ -88,10 +92,76 @@ func TestKubeTar2TFS3(t *testing.T) {
 
 	expectedRecords, err := core.NewTFRecordReader(tfRecord).ReadAllExamples()
 	tassert.CheckFatal(t, err)
-	actualRecords, err := core.NewTFRecordReader(outFileGet).ReadAllExamples()
+	actualRecords, err := core.NewTFRecordReader(outFileBuffer).ReadAllExamples()
 	tassert.CheckFatal(t, err)
 
 	equal, err := tfRecordsEqual(expectedRecords, actualRecords)
 	tassert.CheckFatal(t, err)
 	tassert.Errorf(t, equal == true, "actual and expected records different")
+}
+
+func TestKubeTar2TFRanges(t *testing.T) {
+	tutils.CheckSkip(t, tutils.SkipTestArgs{Kubernetes: true})
+
+	type testCase struct {
+		start, end int64
+	}
+
+	var (
+		tarObjName = "small-mnist-3.tar"
+		tarPath    = filepath.Join("data", tarObjName)
+		proxyURL   = tutils.RandomProxyURL()
+		bck        = cmn.Bck{
+			Name:     TestBucketName,
+			Provider: cmn.ProviderAIS,
+		}
+		baseParams     = tutils.BaseAPIParams(proxyURL)
+		rangeBytesBuff = bytes.NewBuffer(nil)
+
+		tcs = []testCase{
+			{start: 0, end: 1},
+			{start: 0, end: 50},
+			{start: 1, end: 20},
+			{start: 15, end: 100},
+			{start: 120, end: 240},
+			{start: 123, end: 1234},
+		}
+	)
+
+	tutils.CreateFreshBucket(t, proxyURL, bck)
+	defer tutils.DestroyBucket(t, proxyURL, bck)
+
+	// PUT TAR to the cluster
+	f, err := readers.NewFileReaderFromFile(tarPath, cmn.ChecksumXXHash)
+	tassert.CheckFatal(t, err)
+	putArgs := api.PutObjectArgs{
+		BaseParams: baseParams,
+		Bck:        bck,
+		Object:     tarObjName,
+		Cksum:      f.Cksum(),
+		Reader:     f,
+	}
+	tassert.CheckFatal(t, api.PutObject(putArgs))
+
+	uuid := startTar2TfTransformer(t)
+	defer func() {
+		tassert.CheckFatal(t, api.TransformStop(baseParams, uuid))
+	}()
+
+	// GET TFRecord from TAR
+	wholeTFRecord := bytes.NewBuffer(nil)
+	_, err = api.GetObjectS3(baseParams, bck, tarObjName+"!"+uuid, api.GetObjectInput{Writer: wholeTFRecord})
+	tassert.CheckFatal(t, err)
+
+	for _, tc := range tcs {
+		rangeBytesBuff.Reset()
+
+		// Request only a subset of bytes
+		header := http.Header{}
+		header.Set(cmn.HeaderRange, fmt.Sprintf("bytes=%d-%d", tc.start, tc.end))
+		_, err = api.GetObjectS3(baseParams, bck, tarObjName+"!"+uuid, api.GetObjectInput{Writer: rangeBytesBuff, Header: header})
+		tassert.CheckFatal(t, err)
+
+		tassert.Errorf(t, bytes.Equal(rangeBytesBuff.Bytes(), wholeTFRecord.Bytes()[tc.start:tc.end+1]), "[start: %d, end: %d] bytes different", tc.start, tc.end)
+	}
 }
