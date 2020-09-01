e00626f58f47a60be75940cc854fec5e25ff84ba
Janusz Marcinkiewicz <januszm@nvidia.com>
2020-09-01 10:22:22 +0200

    scripts: fix cloud settings for cleanup script

--- deploy/scripts/clean_deploy.sh
+++ deploy/scripts/clean_deploy.sh
@@ -5,7 +5,7 @@ set -e
 root_dir="$GOPATH/src/github.com/NVIDIA/aistore"
 
 # Default values
-cloud=0 # no cloud by default
+cloud="n\nn\nn\n" # no cloud by default
 targets=5
 proxies=5
 next_tier=""
@@ -15,7 +15,7 @@ export AIS_NODE_FLAGS="-skip_startup"
 
 while (( "$#" )); do
   case "${1}" in
-    --cloud) cloud=$2; shift; shift;;
+    --cloud) cloud="y\ny\nn\n"; shift;;
     --dir) root_dir=$2; shift; shift;;
     --debug) export AIS_DEBUG=$2; shift; shift;;
     --tier) next_tier="true"; shift;;
