93ad18f7dc54f0c7c09fede36506dea26ee852ab
Vladimir Markelov <vmarkelov@nvidia.com>
2020-09-22 19:19:37 -0700

    improve HrwTargetList function (allocation/speed)

--- cluster/hrw.go
+++ cluster/hrw.go
@@ -7,7 +7,6 @@ package cluster
 import (
 	"errors"
 	"fmt"
-	"sort"
 
 	"github.com/NVIDIA/aistore/cmn"
 	"github.com/NVIDIA/aistore/fs"
@@ -37,18 +36,18 @@ func (e *NoNodesError) Error() string {
 	return fmt.Sprintf("no available targets, %s%s", e.smap.StringEx(), skip)
 }
 
-// Requires elements of smap.Tmap to have their idDigest initialized
+// Returns the target with highest HRW that is "available"(e.g, is not in suspend list).
 func HrwTarget(uname string, smap *Smap) (si *Snode, err error) {
 	var (
 		max    uint64
 		digest = xxhash.ChecksumString64S(uname, cmn.MLCG32)
 	)
-	for _, sinfo := range smap.Tmap {
+	for _, tsi := range smap.Tmap {
 		// Assumes that sinfo.idDigest is initialized
-		cs := xoshiro256.Hash(sinfo.idDigest ^ digest)
+		cs := xoshiro256.Hash(tsi.idDigest ^ digest)
 		if cs >= max {
 			max = cs
-			si = sinfo
+			si = tsi
 		}
 	}
 	if si == nil {
@@ -57,58 +56,83 @@ func HrwTarget(uname string, smap *Smap) (si *Snode, err error) {
 	return
 }
 
+// Utility struct to generate a list of N first Snodes sorted by their weight
+type hrwList struct {
+	hs  []uint64
+	sis Nodes
+	n   int
+}
+
+func newHrwList(count int) *hrwList {
+	return &hrwList{hs: make([]uint64, 0, count), sis: make(Nodes, 0, count), n: count}
+}
+
+// Adds Snode with `weight`. The result is sorted on the fly with insertion sort
+// and it makes sure that the length of resulting list never exceeds `count`
+func (hl *hrwList) add(weight uint64, sinfo *Snode) {
+	l := len(hl.sis)
+	if l == hl.n && weight <= hl.hs[l-1] {
+		return
+	}
+	if l == hl.n {
+		hl.hs[l-1] = weight
+		hl.sis[l-1] = sinfo
+	} else {
+		hl.hs = append(hl.hs, weight)
+		hl.sis = append(hl.sis, sinfo)
+		l++
+	}
+	idx := l - 1
+	for idx > 0 && hl.hs[idx-1] < hl.hs[idx] {
+		hl.hs[idx], hl.hs[idx-1] = hl.hs[idx-1], hl.hs[idx]
+		hl.sis[idx], hl.sis[idx-1] = hl.sis[idx-1], hl.sis[idx]
+		idx--
+	}
+}
+
+func (hl *hrwList) get() Nodes {
+	return hl.sis
+}
+
 // Sorts all targets in a cluster by their respective HRW (weights) in a descending order;
 // returns resulting subset (aka slice) that has the requested length = count.
 // Returns error if the cluster does not have enough targets.
-//
-// TODO: avoid allocating `arr`
+// If count == length of Smap.Tmap, the function returns as many targets as possible.
 func HrwTargetList(uname string, smap *Smap, count int) (sis Nodes, err error) {
 	cnt := smap.CountTargets()
 	if cnt < count {
 		err = fmt.Errorf("insufficient targets: required %d, available %d, %s", count, cnt, smap)
 		return
 	}
-	var (
-		arr = make([]struct {
-			node *Snode
-			hash uint64
-		}, cnt)
-		digest = xxhash.ChecksumString64S(uname, cmn.MLCG32)
-		i      int
-	)
-	sis = make(Nodes, count)
+	digest := xxhash.ChecksumString64S(uname, cmn.MLCG32)
+	hlist := newHrwList(count)
+
 	for _, sinfo := range smap.Tmap {
 		cs := xoshiro256.Hash(sinfo.idDigest ^ digest)
-		arr[i] = struct {
-			node *Snode
-			hash uint64
-		}{sinfo, cs}
-		i++
+		hlist.add(cs, sinfo)
 	}
-	sort.Slice(arr, func(i, j int) bool { return arr[i].hash > arr[j].hash })
-	for i := 0; i < count; i++ {
-		sis[i] = arr[i].node
+	sis = hlist.get()
+	if count != cnt && len(sis) < count {
+		err = fmt.Errorf("insufficient targets: required %d, available %d, %s", count, len(sis), smap)
+		return nil, err
 	}
-	return
+	return sis, nil
 }
 
 func HrwProxy(smap *Smap, idToSkip string) (pi *Snode, err error) {
 	var (
-		max     uint64
-		skipped int
+		max uint64
 	)
-	for id, sinfo := range smap.Pmap {
-		if id == idToSkip {
-			skipped++
+	for pid, psi := range smap.Pmap {
+		if pid == idToSkip {
 			continue
 		}
-		if _, ok := smap.NonElects[id]; ok {
-			skipped++
+		if _, ok := smap.NonElects[pid]; ok {
 			continue
 		}
-		if sinfo.idDigest >= max {
-			max = sinfo.idDigest
-			pi = sinfo
+		if psi.idDigest >= max {
+			max = psi.idDigest
+			pi = psi
 		}
 	}
 	if pi == nil {
