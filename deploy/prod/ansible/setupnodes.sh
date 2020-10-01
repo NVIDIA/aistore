0e84252e012099b66af53343e78053e8b1bacca1
Prashanth Dintyala <saiprashanth173@gmail.com>
2020-09-30 18:22:28 -0700

    Ansible script fixes

--- deploy/prod/ansible/setupnodes.sh
+++ deploy/prod/ansible/setupnodes.sh
@@ -7,7 +7,7 @@ echo "Add GOPATH and GOBIN"
 sudo touch /etc/profile.d/aispaths.sh
 sudo sh -c 'cat << EOF > /etc/profile.d/aispaths.sh
 export GOBIN=$HOME/ais/bin
-export GOPATH=$HOME/ais/
+export GOPATH=$HOME/ais
 export PATH=$PATH:/usr/local/go/bin:$GOBIN
 export AISSRC=$HOME/ais/src/github.com/NVIDIA/aistore/ais
 export AISTORE_SRC=$HOME/ais/src/github.com/NVIDIA/aistore
@@ -16,25 +16,31 @@ EOF'
 sudo chmod 777 /etc/profile.d/aispaths.sh
 . /etc/profile.d/aispaths.sh
 
-rm -rf ~/ais || true
+sudo rm -rf ~/ais || true
 mkdir -p ~/ais/{bin,pkg,src}
 
+GOLANG_VER_FILE="/usr/local/go/VERSION"
 GOLANG_VERSION="go1.13"
-CURRENT_GOLANG_VERSION=$(cat /usr/local/go/VERSION)
-if [[ ${CURRENT_GOLANG_VERSION} != ${GOLANG_VERSION} ]]; then
-    echo "Current Golang version does not match with expected, so updating Golang to " ${GOLANG_VERSION}
-    sudo rm -rf /usr/local/go
-    sudo rm -rf /use/bin/go
-    echo "Downloading Go..."
-    curl -LO https://storage.googleapis.com/golang/${GOLANG_VERSION}.linux-amd64.tar.gz
-    shasum -a 256 go1.*
-    sudo tar -C /usr/local -xvzf ${GOLANG_VERSION}.linux-amd64.tar.gz > /dev/null
-    sudo ln -s /usr/local/go/bin/go /usr/bin/go
-    rm -rf ${GOLANG_VERSION}.linux-amd64.tar.gz
+CURRENT_GOLANG_VERSION=""
+if [[ -f ${GOLANG_VER_FILE} ]]; then
+  CURRENT_GOLANG_VERSION=$(cat ${GOLANG_VER_FILE})
 fi
 
-echo "Getting AIStore source..."
-go get -v github.com/NVIDIA/aistore
+if [[ ${CURRENT_GOLANG_VERSION} != "${GOLANG_VERSION}" ]]; then
+  echo "Current Golang version does not match with expected, so updating Golang to " ${GOLANG_VERSION}
+  sudo rm -rf /usr/local/go /usr/bin/go
+  echo "Downloading Go..."
+  curl -LO https://storage.googleapis.com/golang/${GOLANG_VERSION}.linux-amd64.tar.gz
+  shasum -a 256 go1.*
+  sudo tar -C /usr/local -xvzf ${GOLANG_VERSION}.linux-amd64.tar.gz >/dev/null
+  sudo ln -s /usr/local/go/bin/go /usr/bin/go
+  rm -rf ${GOLANG_VERSION}.linux-amd64.tar.gz
+fi
+
+GIT_NVIDIA=${GOPATH}/src/github.com/NVIDIA
+mkdir -p "${GIT_NVIDIA}"
+cd "${GIT_NVIDIA}"
+git clone https://github.com/NVIDIA/aistore.git
 
 echo "Cloud provider(s) set to: ${AIS_CLD_PROVIDERS}"
-cd ${AISTORE_SRC} && make node
+cd aistore && make node
