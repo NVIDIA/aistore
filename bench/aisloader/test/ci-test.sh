cf44c8bd092b723e5d3f336e0446230a4d897827
Janusz Marcinkiewicz <januszm@nvidia.com>
2020-10-08 09:57:48 +0200

    ci: fix aisloader reader type when using ETL

--- bench/aisloader/test/ci-test.sh
+++ bench/aisloader/test/ci-test.sh
@@ -26,16 +26,14 @@ case $i in
   esac
 done
 
-if [[ -z $bucket_name ]]; then
+if [[ -z ${bucket_name} ]]; then
   bucket_name=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
 fi
 
 if [ "$etl" = true ]; then
-  # TODO: possibly we could do everything in one call.
-  # However, aisloader teminates immediately, if there is no objects in the bucket and pctput != 100.
-  aisloader -bucket="$bucket_name" -duration="$duration" -pctput=100 -provider=ais -maxsize=10Mib -minsize=1Mib -totalputsize=5Gib -cleanup=false -readertype=tar -numworkers=8
-  aisloader -bucket="$bucket_name" -duration="$duration" -pctput=10 -provider=ais  -maxsize=10Mib -minsize=1Mib -totalputsize=5Gib -cleanup=true -numworkers=8 -etl=tar2tf
+  aisloader -bucket="${bucket_name}" -duration="$duration" -pctput=100 -provider=ais -maxsize=10Mib -minsize=1Mib -totalputsize=5Gib -cleanup=false -numworkers=8 -readertype=tar
+  aisloader -bucket="${bucket_name}" -duration="$duration" -pctput=10 -provider=ais  -maxsize=10Mib -minsize=1Mib -totalputsize=5Gib -cleanup=true  -numworkers=8 -readertype=tar -etl=tar2tf
 else
-  aisloader -bucket="$bucket_name" -duration="$duration" -pctput=100 -provider=ais  -maxsize=10Mib -minsize=1Mib -totalputsize=10Gib -cleanup=false -numworkers=8
-  aisloader -bucket="$bucket_name" -duration="$duration" -pctput=10 -provider=ais  -maxsize=10Mib -minsize=1Mib -totalputsize=10Gib -cleanup=true -numworkers=8
+  aisloader -bucket="${bucket_name}" -duration="$duration" -pctput=100 -provider=ais -maxsize=10Mib -minsize=1Mib -totalputsize=10Gib -cleanup=false -numworkers=8
+  aisloader -bucket="${bucket_name}" -duration="$duration" -pctput=10 -provider=ais  -maxsize=10Mib -minsize=1Mib -totalputsize=10Gib -cleanup=true  -numworkers=8
 fi
