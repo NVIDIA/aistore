1b048ccc32c5e8a342454c4fd608aa2aff398c19
Janusz Marcinkiewicz <januszm@nvidia.com>
2021-01-14 12:33:37 +0100

    cmn: move code to match order of providers
    Signed-off-by: Janusz Marcinkiewicz <januszm@nvidia.com>

--- cmn/cloud.go
+++ cmn/cloud.go
@@ -27,43 +27,12 @@ func awsIsVersionSet(version *string) bool {
 }
 
 var CloudHelpers = struct {
-	Google cloudFuncs
 	Amazon cloudFuncs
 	Azure  cloudFuncs
+	Google cloudFuncs
 	HDFS   cloudFuncs
 	HTTP   cloudFuncs
 }{
-	Google: cloudFuncs{
-		EncodeVersion: func(v interface{}) (string, bool) {
-			switch x := v.(type) {
-			case string:
-				return x, x != ""
-			case int64:
-				return strconv.FormatInt(x, 10), true
-			default:
-				panic(v)
-			}
-		},
-		EncodeCksum: func(v interface{}) (string, bool) {
-			switch x := v.(type) {
-			case string:
-				decoded, err := base64.StdEncoding.DecodeString(x)
-				if err != nil {
-					return "", false
-				}
-				return hex.EncodeToString(decoded), true
-			case []byte:
-				return hex.EncodeToString(x), true
-			case uint32:
-				// Encode a uint32 as Base64 in big-endian byte order.
-				// See: https://cloud.google.com/storage/docs/xml-api/reference-headers#xgooghash.
-				b := []byte{byte(x >> 24), byte(x >> 16), byte(x >> 8), byte(x)}
-				return base64.StdEncoding.EncodeToString(b), true
-			default:
-				panic(v)
-			}
-		},
-	},
 	Amazon: cloudFuncs{
 		EncodeVersion: func(v interface{}) (string, bool) {
 			switch x := v.(type) {
@@ -115,6 +84,37 @@ var CloudHelpers = struct {
 				return hex.EncodeToString(decoded), true
 			case []byte:
 				return hex.EncodeToString(x), true
+			default:
+				panic(v)
+			}
+		},
+	},
+	Google: cloudFuncs{
+		EncodeVersion: func(v interface{}) (string, bool) {
+			switch x := v.(type) {
+			case string:
+				return x, x != ""
+			case int64:
+				return strconv.FormatInt(x, 10), true
+			default:
+				panic(v)
+			}
+		},
+		EncodeCksum: func(v interface{}) (string, bool) {
+			switch x := v.(type) {
+			case string:
+				decoded, err := base64.StdEncoding.DecodeString(x)
+				if err != nil {
+					return "", false
+				}
+				return hex.EncodeToString(decoded), true
+			case []byte:
+				return hex.EncodeToString(x), true
+			case uint32:
+				// Encode a uint32 as Base64 in big-endian byte order.
+				// See: https://cloud.google.com/storage/docs/xml-api/reference-headers#xgooghash.
+				b := []byte{byte(x >> 24), byte(x >> 16), byte(x >> 8), byte(x)}
+				return base64.StdEncoding.EncodeToString(b), true
 			default:
 				panic(v)
 			}
