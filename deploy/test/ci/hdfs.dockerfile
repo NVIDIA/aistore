b0af06ce389cf27b73507e8cbbeac4e249e7bc6f
Janusz Marcinkiewicz <januszm@nvidia.com>
2021-02-12 14:12:41 +0100

    ci: fix setup in HDFS job
    Signed-off-by: Janusz Marcinkiewicz <januszm@nvidia.com>

--- deploy/test/ci/hdfs.dockerfile
+++ deploy/test/ci/hdfs.dockerfile
@@ -3,13 +3,20 @@ FROM ubuntu:16.04
 ENV GOPATH="/go"
 ENV PATH="${GOPATH}/bin:/usr/local/go/bin:${PATH}"
 ENV GOLANG_VERSION="1.15"
+ENV HADOOP_VERSION="3.3.0"
 
 RUN apt-get update -yq
 RUN apt-get --no-install-recommends -y install lsb-release sudo default-jre default-jdk
 RUN apt-get --no-install-recommends -y install curl git sysstat attr build-essential lsof fuse coreutils
+RUN apt-get --no-install-recommends -y install openjdk-8-jdk ssh openssh-server
 
 # Install Go.
 RUN mkdir -p "$GOPATH/bin" && chmod -R 777 "$GOPATH"
 RUN curl -LO https://storage.googleapis.com/golang/go${GOLANG_VERSION}.linux-amd64.tar.gz &&\
-  tar -C /usr/local -xvzf go${GOLANG_VERSION}.linux-amd64.tar.gz > /dev/null 2>&1 &&\
+  tar -C /usr/local -xzf go${GOLANG_VERSION}.linux-amd64.tar.gz > /dev/null 2>&1 &&\
   rm -rf go${GOLANG_VERSION}.linux-amd64.tar.gz
+
+# Install Hadoop.
+RUN curl -LO  http://mirrors.sonic.net/apache/hadoop/common/hadoop-${HADOOP_VERSION}/hadoop-${HADOOP_VERSION}.tar.gz &&\
+  tar -xzf hadoop-${HADOOP_VERSION}.tar.gz > /dev/null 2>&1 &&\
+  rm -rf hadoop-${HADOOP_VERSION}.tar.gz
--- deploy/test/ci/setup_hdfs.sh
+++ deploy/test/ci/setup_hdfs.sh
@@ -1,78 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
+set -x
 
-UBUNTU_CODENAME=$(lsb_release -c | awk '{print $2}')
+# Based on: https://hadoop.apache.org/docs/current/hadoop-project-dist/hadoop-common/SingleCluster.html
 
-sudo tee /etc/apt/sources.list.d/cdh.list <<EOF
-deb [arch=amd64] http://archive.cloudera.com/cdh5/ubuntu/$UBUNTU_CODENAME/amd64/cdh $UBUNTU_CODENAME-cdh5 contrib
+# Setup ssh server and keys.
+/etc/init.d/ssh start
+mkdir ~/.ssh
+ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -P ""
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+chmod 0600 ~/.ssh/authorized_keys
+
+pushd /hadoop-3.3.0 || exit 1
+
+cat <<EOF >> etc/hadoop/hadoop-env.sh
+export HDFS_NAMENODE_USER="root"
+export HDFS_DATANODE_USER="root"
+export HDFS_SECONDARYNAMENODE_USER="root"
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 EOF
 
-sudo tee /etc/apt/preferences.d/cloudera.pref <<EOF
-Package: *
-Pin: release o=Cloudera, l=Cloudera
-Pin-Priority: 501
-EOF
-
-apt-get update
-
-CONF_AUTHENTICATION="simple"
-
-mkdir -p /etc/hadoop/conf.gohdfs
-tee /etc/hadoop/conf.gohdfs/core-site.xml <<EOF
+cat > etc/hadoop/core-site.xml <<EOF
 <configuration>
-  <property>
-    <name>fs.defaultFS</name>
-    <value>hdfs://localhost:9000</value>
-  </property>
-  <property>
-    <name>hadoop.security.authentication</name>
-    <value>$CONF_AUTHENTICATION</value>
-  </property>
+    <property>
+        <name>fs.defaultFS</name>
+        <value>hdfs://localhost:9000</value>
+    </property>
 </configuration>
 EOF
 
-tee /etc/hadoop/conf.gohdfs/hdfs-site.xml <<EOF
+cat > etc/hadoop/hdfs-site.xml <<EOF
 <configuration>
-  <property>
-    <name>dfs.namenode.name.dir</name>
-    <value>/opt/hdfs/name</value>
-  </property>
-  <property>
-    <name>dfs.datanode.data.dir</name>
-    <value>/opt/hdfs/data</value>
-  </property>
-  <property>
-   <name>dfs.permissions.enabled</name>
-   <value>false</value>
-  </property>
-  <property>
-   <name>dfs.permissions.superusergroup</name>
-   <value>hadoop</value>
-  </property>
-  <property>
-    <name>dfs.safemode.extension</name>
-    <value>0</value>
-  </property>
-  <property>
-     <name>dfs.safemode.min.datanodes</name>
-     <value>1</value>
-  </property>
-  <property>
-    <name>ignore.secure.ports.for.testing</name>
-    <value>true</value>
-  </property>
+    <property>
+        <name>dfs.replication</name>
+        <value>1</value>
+    </property>
 </configuration>
 EOF
 
-update-alternatives --install /etc/hadoop/conf hadoop-conf /etc/hadoop/conf.gohdfs 99
-apt-get install -y --no-install-recommends --allow-unauthenticated hadoop-hdfs-namenode hadoop-hdfs-datanode
+bin/hdfs namenode -format
+sbin/start-dfs.sh
 
-mkdir -p /opt/hdfs/data /opt/hdfs/name
-chown -R hdfs:hdfs /opt/hdfs
-sudo -u hdfs hdfs namenode -format -nonInteractive
-
-sudo service hadoop-hdfs-datanode restart
-sudo service hadoop-hdfs-namenode restart
-
-hdfs dfsadmin -safemode wait
+popd || exit 1
