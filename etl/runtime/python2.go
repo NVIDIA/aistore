6d3f3793f7d7bc91f9a10ec673ad5fa8399caedf
Janusz Marcinkiewicz <januszm@nvidia.com>
2021-05-08 20:57:02 +0200

    general: use `embed` package
    Signed-off-by: Janusz Marcinkiewicz <januszm@nvidia.com>

--- etl/runtime/python2.go
+++ etl/runtime/python2.go
@@ -4,65 +4,17 @@
  */
 package runtime
 
+import _ "embed"
+
 type (
 	// py2 implements Runtime for "python2".
 	py2 struct{}
 )
 
+//go:embed python2.yaml
+var py2PodSpec string
+
 func (r py2) Type() string        { return Python2 }
 func (r py2) CodeEnvName() string { return "AISTORE_CODE" }
 func (r py2) DepsEnvName() string { return "AISTORE_DEPS" }
-func (r py2) PodSpec() string {
-	return `
-apiVersion: v1
-kind: Pod
-metadata:
-  name: <NAME>
-spec:
-  containers:
-    - name: server
-      image: aistore/runtime_python:2
-      imagePullPolicy: Always
-      ports:
-        - name: default
-          containerPort: 80
-      command: ['sh', '-c', 'python /server.py']
-      env:
-        - name: MOD_NAME
-          value: code
-        - name: FUNC_HANDLER
-          value: transform
-        - name: PYTHONPATH
-          value: /runtime
-      readinessProbe:
-        httpGet:
-          path: /health
-          port: default
-      volumeMounts:
-        - name: code
-          mountPath: "/code"
-        - name: runtime
-          mountPath: "/runtime"
-  initContainers:
-    - name: server-deps
-      image: aistore/runtime_python:2
-      imagePullPolicy: Always
-      command:
-        - 'sh'
-        - '-c'
-        - | 
-          echo "${AISTORE_CODE}" > /dst/code.py
-          echo "${AISTORE_DEPS}" > /dst/requirements.txt
-          pip install --target="/runtime" -r /dst/requirements.txt
-      volumeMounts:
-        - name: code
-          mountPath: "/dst"
-        - name: runtime
-          mountPath: "/runtime"
-  volumes:
-    - name: code
-      emptyDir: {}
-    - name: runtime
-      emptyDir: {}
-`
-}
+func (r py2) PodSpec() string     { return py2PodSpec }
--- etl/runtime/python3.go
+++ etl/runtime/python3.go
@@ -4,65 +4,17 @@
  */
 package runtime
 
+import _ "embed"
+
 type (
 	// py3 implements Runtime for "python3".
 	py3 struct{}
 )
 
+//go:embed python3.yaml
+var py3PodSpec string
+
 func (r py3) Type() string        { return Python3 }
 func (r py3) CodeEnvName() string { return "AISTORE_CODE" }
 func (r py3) DepsEnvName() string { return "AISTORE_DEPS" }
-func (r py3) PodSpec() string {
-	return `
-apiVersion: v1
-kind: Pod
-metadata:
-  name: <NAME>
-spec:
-  containers:
-    - name: server
-      image: aistore/runtime_python:3
-      imagePullPolicy: Always
-      ports:
-        - name: default
-          containerPort: 80
-      command: ['sh', '-c', 'python /server.py']
-      env:
-        - name: MOD_NAME
-          value: code
-        - name: FUNC_HANDLER
-          value: transform
-        - name: PYTHONPATH
-          value: /runtime
-      readinessProbe:
-        httpGet:
-          path: /health
-          port: default
-      volumeMounts:
-        - name: code
-          mountPath: "/code"
-        - name: runtime
-          mountPath: "/runtime"
-  initContainers:
-    - name: server-deps
-      image: aistore/runtime_python:3
-      imagePullPolicy: Always
-      command:
-        - 'sh'
-        - '-c'
-        - | 
-          echo "${AISTORE_CODE}" > /dst/code.py
-          echo "${AISTORE_DEPS}" > /dst/requirements.txt
-          pip install --target="/runtime" -r /dst/requirements.txt
-      volumeMounts:
-        - name: code
-          mountPath: "/dst"
-        - name: runtime
-          mountPath: "/runtime"
-  volumes:
-    - name: code
-      emptyDir: {}
-    - name: runtime
-      emptyDir: {}
-`
-}
+func (r py3) PodSpec() string     { return py3PodSpec }
