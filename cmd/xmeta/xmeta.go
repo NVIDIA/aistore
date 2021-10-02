79d72e21c0e8182fedc99f1ed5ca8fa863ce76f7
Alex Aizman <alex.aizman@gmail.com>
2021-10-02 11:30:27 -0400

    xmeta (tool): add VMD, refactor
    Signed-off-by: Alex Aizman <alex.aizman@gmail.com>

--- cmd/xmeta/xmeta.go
+++ cmd/xmeta/xmeta.go
@@ -16,6 +16,7 @@ import (
 	"github.com/NVIDIA/aistore/cmn"
 	"github.com/NVIDIA/aistore/cmn/cos"
 	"github.com/NVIDIA/aistore/cmn/jsp"
+	"github.com/NVIDIA/aistore/fs"
 	jsoniter "github.com/json-iterator/go"
 )
 
@@ -31,36 +32,65 @@ const (
 
 Examples:
 	xmeta -h                                          - show usage
+	# Smap:
 	xmeta -x -in=~/.ais0/.ais.smap                    - extract Smap to STDOUT
 	xmeta -x -in=~/.ais0/.ais.smap -out=/tmp/smap.txt - extract Smap to /tmp/smap.txt
 	xmeta -in=/tmp/smap.txt -out=/tmp/.ais.smap       - format plain-text /tmp/smap.txt
+	# BMD:
 	xmeta -x -in=~/.ais0/.ais.bmd                     - extract BMD to STDOUT
 	xmeta -x -in=~/.ais0/.ais.bmd -out=/tmp/bmd.txt   - extract BMD to /tmp/bmd.txt
 	xmeta -in=/tmp/bmd.txt -out=/tmp/.ais.bmd         - format plain-text /tmp/bmd.txt
+	# RMD:
 	xmeta -x -in=~/.ais0/.ais.rmd                     - extract RMD to STDOUT
 	xmeta -x -in=~/.ais0/.ais.rmd -out=/tmp/rmd.txt   - extract RMD to /tmp/rmd.txt
 	xmeta -in=/tmp/rmd.txt -out=/tmp/.ais.rmd         - format plain-text /tmp/rmd.txt
+	# Config:
 	xmeta -x -in=~/.ais0/.ais.conf                    - extract Config to STDOUT
-	xmeta -x -in=~/.ais0/.ais.conf -out=/tmp/conf.txt - extract Config to /tmp/c.txt
-	xmeta -in=/tmp/conf.txt -out=/tmp/.ais.conf       - format plain-text /tmp/c.txt
+	xmeta -x -in=~/.ais0/.ais.conf -out=/tmp/conf.txt - extract Config to /tmp/config.txt
+	xmeta -in=/tmp/conf.txt -out=/tmp/.ais.conf       - format plain-text /tmp/config.txt
+	# VMD:
+	xmeta -x -in=~/.ais0/.ais.vmd                     - extract VMD to STDOUT
+	xmeta -x -in=~/.ais0/.ais.vmd -out=/tmp/vmd.txt   - extract VMD to /tmp/vmd.txt
+	xmeta -in=/tmp/vmd.txt -out=/tmp/.ais.vmd         - format plain-text /tmp/vmd.txt
 `
 )
 
+var m = map[string]struct {
+	extract func() error
+	format  func() error
+	what    string
+}{
+	"smap": {extractSmap, formatSmap, "Smap"},
+	"bmd":  {extractBMD, formatBMD, "BMD"},
+	"rmd":  {extractRMD, formatRMD, "RMD"},
+	"conf": {extractConfig, formatConfig, "Config"},
+	"vmd":  {extractVMD, formatVMD, "VMD"},
+}
+
+// "extract*" routines expect AIS-formatted (smap, bmd, rmd, etc.)
+
+func extractSmap() error   { return extractMeta(&cluster.Smap{}) }
+func extractBMD() error    { return extractMeta(&cluster.BMD{}) }
+func extractRMD() error    { return extractMeta(&cluster.RMD{}) }
+func extractConfig() error { return extractMeta(&cmn.ClusterConfig{}) }
+func extractVMD() error    { return extractMeta(&fs.VMD{}) }
+
+// "format*" routines require output filename
+
+func formatSmap() error   { return formatMeta(&cluster.Smap{}) }
+func formatBMD() error    { return formatMeta(&cluster.BMD{}) }
+func formatRMD() error    { return formatMeta(&cluster.RMD{}) }
+func formatConfig() error { return formatMeta(&cmn.ClusterConfig{}) }
+func formatVMD() error    { return formatMeta(&fs.VMD{}) }
+
 func main() {
-	var (
-		f          func() error
-		what, verb string
-	)
-
 	newFlag := flag.NewFlagSet(os.Args[0], flag.ExitOnError) // discard flags of imported packages
-
 	newFlag.BoolVar(&flags.extract, "x", false,
-		"true: extract AIS-formatted BMD or Smap, false: format plain-text BMD or Smap for AIS")
+		"true: extract AIS-formatted metadata type, false: pack and AIS-format plain-text metadata")
 	newFlag.StringVar(&flags.in, "in", "", "fully-qualified input filename")
 	newFlag.StringVar(&flags.out, "out", "", "output filename (optional when extracting)")
 	newFlag.BoolVar(&flags.help, "h", false, "print usage and exit")
 	newFlag.Parse(os.Args[1:])
-
 	if flags.help || len(os.Args[1:]) == 0 {
 		fmt.Print(helpMsg)
 		os.Exit(0)
@@ -71,52 +101,36 @@ func main() {
 		flags.out = cmn.ExpandPath(flags.out)
 	}
 	in := strings.ToLower(flags.in)
-	if flags.extract {
-		verb = "extract"
-		if strings.Contains(in, "smap") {
-			f, what = extractSmap, "Smap"
-		} else if strings.Contains(in, "bmd") {
-			f, what = extractBMD, "BMD"
-		} else if strings.Contains(in, "rmd") {
-			f, what = extractRMD, "RMD"
-		} else if strings.Contains(in, "conf") || strings.Contains(in, "ais.json") {
-			f, what = extractConfig, "Global Config"
-		} else if err := extractSmap(); err == nil {
-			return
-		} else {
-			f, what = extractBMD, "BMD"
-		}
-	} else {
-		verb = "format"
-		if strings.Contains(in, "smap") {
-			f, what = formatSmap, "Smap"
-		} else if strings.Contains(in, "bmd") {
-			f, what = formatBMD, "BMD"
-		} else if strings.Contains(in, "rmd") {
-			f, what = formatRMD, "RMD"
-		} else if strings.Contains(in, "conf") || strings.Contains(in, "ais.json") {
-			f, what = formatConfig, "Global Config"
-		} else if err := formatSmap(); err == nil {
-			return
-		} else {
-			f, what = formatBMD, "BMD"
-		}
-	}
+	f, what := parse(in, flags.extract)
 	if err := f(); err != nil {
 		if flags.extract {
-			fmt.Printf("Failed to %s %s from %s: %v\n", verb, what, in, err)
+			fmt.Printf("Failed to extract %s from %s: %v\n", what, in, err)
 		} else {
-			fmt.Printf("Cannot %s %s: plain-text input %s, error=\"%v\"\n", verb, what, in, err)
+			fmt.Printf("Cannot format %s: plain-text input %s, error=\"%v\"\n", what, in, err)
 		}
 	}
 }
 
-// "extract*" routines expect AIS-formatted (smap, bmd, rmd, etc.)
-
-func extractSmap() error   { return extractMeta(&cluster.Smap{}) }
-func extractBMD() error    { return extractMeta(&cluster.BMD{}) }
-func extractRMD() error    { return extractMeta(&cluster.RMD{}) }
-func extractConfig() error { return extractMeta(&cmn.ClusterConfig{}) }
+func parse(in string, extract bool) (f func() error, what string) {
+	var all []string
+	for k, e := range m {
+		if !strings.Contains(in, k) {
+			all = append(all, e.what)
+			continue
+		}
+		if extract {
+			f, what = e.extract, e.what
+		} else {
+			f, what = e.format, e.what
+		}
+		break
+	}
+	if what == "" {
+		fmt.Printf("Failed to parse %q for AIS metadata type, one of: %q\n", in, all)
+		os.Exit(1)
+	}
+	return
+}
 
 func extractMeta(v jsp.Opts) (err error) {
 	f := os.Stdout
@@ -134,13 +148,6 @@ func extractMeta(v jsp.Opts) (err error) {
 	_, err = fmt.Fprintln(f, string(s))
 	return err
 }
-
-// "format*" routines require output filename
-
-func formatSmap() error   { return formatMeta(&cluster.Smap{}) }
-func formatBMD() error    { return formatMeta(&cluster.BMD{}) }
-func formatRMD() error    { return formatMeta(&cluster.RMD{}) }
-func formatConfig() error { return formatMeta(&cmn.ClusterConfig{}) }
 
 func formatMeta(v jsp.Opts) error {
 	if flags.out == "" {
