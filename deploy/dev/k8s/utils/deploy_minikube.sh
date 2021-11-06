b0d47f758c6a61c0a3658c3bd4f3d67589d4ea2e
Alex Aizman <alex.aizman@gmail.com>
2021-11-06 12:52:32 -0400

    follow-up
    Signed-off-by: Alex Aizman <alex.aizman@gmail.com>

--- deploy/dev/k8s/utils/deploy_minikube.sh
+++ deploy/dev/k8s/utils/deploy_minikube.sh
@@ -33,7 +33,8 @@ if [[ "$metrics" == "y" ]]; then
   # NOTE: Taken from https://github.com/prometheus-operator/kube-prometheus#quickstart.
   # Create the namespace and CRDs, and then wait for them to be available before creating the remaining resources.
   kubectl create -f manifests/setup
-  until kubectl get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
+  # timeout = 300s
+  for i in {1..30}; do if `kubectl get servicemonitors --all-namespaces`; then break; fi; sleep 10; done
   kubectl create -f manifests/
   popd
 
