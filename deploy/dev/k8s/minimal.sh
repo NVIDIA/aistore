f234b5ed6db25ec834fc01e0e927d48ad970240f
Abhishek Gaikwad <gaikwadabhishek1997@gmail.com>
2022-07-25 18:05:02 -0700

    deploy/k8s: fixes and created minimal minikube deployment
    Signed-off-by: Abhishek Gaikwad <gaikwadabhishek1997@gmail.com>

---
+++ deploy/dev/k8s/minimal.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+
+source utils/ais_minikube_setup.sh
+export AIS_FS_PATHS=""
+export TEST_FSPATH_COUNT=1
+# TODO: replace with official docker aisnode:minikube once released 
+export DOCKER_IMAGE="gaikwadabhishek/aisnode:minikube"
+
+PRIMARY_PORT=8080
+HOST_URL="http://$(minikube ip):${PRIMARY_PORT}"
+
+export AIS_PRIMARY_URL=$HOST_URL
+export HOSTNAME_LIST="$(minikube ip)"
+export AIS_BACKEND_PROVIDERS=""
+export TARGET_CNT=1
+
+export POD_NAME="ais-proxy-0"
+export PORT=$PRIMARY_PORT
+export AIS_IS_PRIMARY=true
+
+([[ $(kubectl get pods | grep -c "${POD_NAME}") -gt 0 ]] && kubectl delete pods ${POD_NAME}) || true
+envsubst < kube_templates/aisproxy_deployment.yml | kubectl apply -f -
+
+echo "Waiting for the primary proxy to be ready..."
+kubectl wait --for="condition=ready" --timeout=2m pod ais-proxy-0
+
+echo "Starting target deployment..."
+
+export POD_NAME="ais-target-0"
+export PORT=9090
+export PORT_INTRA_CONTROL=9080
+export PORT_INTRA_DATA=10080
+export TARGET_POS_NUM=1
+
+(minikube ssh "sudo mkdir -p /tmp/${TARGET_POS_NUM}")
+
+([[ $(kubectl get pods | grep -c "${POD_NAME}") -gt 0 ]] && kubectl delete pods ${POD_NAME}) || true
+envsubst < kube_templates/aistarget_deployment.yml | kubectl create -f -
+
+echo "Waiting for the targets to be ready..."
+kubectl wait --for="condition=ready" --timeout=2m pods -l type=aistarget
+
+echo "List of running pods"
+kubectl get pods -o wide
+
+echo "Done."
+echo ""
+
+export AIS_ENDPOINT="http://$(minikube ip):8080"
