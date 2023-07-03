71e2e2c19fc6c3d6383ddbfc6c4c709e19524e34
Ryan Koo <rbk65@cornell.edu>
2023-07-03 16:51:21 -0700

    ci: pyaisloader
    Signed-off-by: Ryan Koo <rbk65@cornell.edu>

---
+++ python/pyaisloader/ci-test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied. Please provide a test type: 'short' or 'long'."
+    exit 1
+fi
+
+cd ./pyaisloader
+make install
+export AIS_ENDPOINT="http://localhost:8080"
+
+if [ "$1" == "short" ]
+  then
+    yes "y" | head -n 2 | pyaisloader p -b ais://testpyaisloader -d 15s -min 1mb -max 10mb -s 1gb -w 16 -c False
+    yes "y" | head -n 2 | pyaisloader g -b ais://testpyaisloader -d 15s -min 1mb -max 10mb -s 1gb -w 16 -c False
+    yes "y" | head -n 2 | pyaisloader m -b ais://testpyaisloader -d 15s -min 1mb -max 10mb -w 16 -c True
+elif [ "$1" == "long" ]
+  then
+    yes "y" | head -n 2 | pyaisloader p -b ais://testpyaisloader -d 3m -min 1mb -max 10mb -s 10gb -w 16 -c False
+    yes "y" | head -n 2 | pyaisloader g -b ais://testpyaisloader -d 3m -min 1mb -max 10mb -s 10gb -w 16 -c False
+    yes "y" | head -n 2 | pyaisloader m -b ais://testpyaisloader -d 3m -min 1mb -max 10mb -w 16 -c True
+else
+    echo "Invalid test type: $1. Please provide a valid test type: 'short' or 'long'."
+    exit 1
+fi
