5a3ea624a35e8020daa979550164d5261efbf2d1
Ryan Koo <rbk65@cornell.edu>
2023-07-26 23:48:11 -0700

    sdk/python: etl templates (imagepullpolicy as always)
    Signed-off-by: Ryan Koo <rbk65@cornell.edu>

--- python/aistore/sdk/etl_templates.py
+++ python/aistore/sdk/etl_templates.py
@@ -1,8 +1,4 @@
-#
-# Copyright (c) 2022-2023, NVIDIA CORPORATION. All rights reserved.
-#
-
-# Returns the MD5 sum of the original data as the response.
+# returns the md5 sum of the original data as the response
 # pylint: disable=unused-variable
 MD5 = """
 apiVersion: v1
@@ -10,16 +6,14 @@ kind: Pod
 metadata:
   name: transformer-md5
   annotations:
-    # Values `communication_type` can take are ["hpull://", "hrev://", "hpush://", "io://"].
-    # Visit https://github.com/NVIDIA/aistore/blob/master/docs/etl.md#communication-mechanisms 
-    # for more details.
+    # Values it can take ["hpull://","hrev://","hpush://"]
     communication_type: "{communication_type}://"
     wait_timeout: 5m
 spec:
   containers:
     - name: server
       image: aistorage/transformer_md5:latest
-      imagePullPolicy: IfNotPresent
+      imagePullPolicy: Always
       ports:
         - name: default
           containerPort: 80
@@ -30,7 +24,7 @@ spec:
           port: default
 """
 
-# Returns "Hello World!" on any request.
+# returns "Hello World!" on any request.
 # pylint: disable=unused-variable
 HELLO_WORLD = """
 apiVersion: v1
@@ -38,16 +32,14 @@ kind: Pod
 metadata:
   name: transformer-hello-world
   annotations:
-    # Values `communication_type` can take are ["hpull://", "hrev://", "hpush://", "io://"].
-    # Visit https://github.com/NVIDIA/aistore/blob/master/docs/etl.md#communication-mechanisms 
-    # for more details.
+    # Values it can take ["hpull://","hrev://","hpush://"]
     communication_type: "{communication_type}://"
     wait_timeout: 5m
 spec:
   containers:
     - name: server
       image: aistorage/transformer_hello_world:latest
-      imagePullPolicy: IfNotPresent
+      imagePullPolicy: Always
       ports:
         - name: default
           containerPort: 80
@@ -58,7 +50,7 @@ spec:
           port: default
 """
 
-# Returns the original data, with an md5 sum in the response headers.
+# returns the original data, with an md5 sum in the response headers.
 # pylint: disable=unused-variable
 GO_ECHO = """
 apiVersion: v1
@@ -66,16 +58,14 @@ kind: Pod
 metadata:
   name: echo-go
   annotations:
-    # Values `communication_type` can take are ["hpull://", "hrev://", "hpush://", "io://"].
-    # Visit https://github.com/NVIDIA/aistore/blob/master/docs/etl.md#communication-mechanisms 
-    # for more details.
+    # Values it can take ["hpull://","hrev://","hpush://"]
     communication_type: "{communication_type}://"
     wait_timeout: 5m
 spec:
   containers:
     - name: server
       image: aistorage/transformer_echo_go:latest
-      imagePullPolicy: IfNotPresent
+      imagePullPolicy: Always
       ports:
         - name: default
           containerPort: 80
@@ -86,7 +76,7 @@ spec:
           port: default
 """
 
-# Returns the original data, with an MD5 sum in the response headers.
+# returns the original data, with an md5 sum in the response headers
 # pylint: disable=unused-variable
 ECHO = """
 apiVersion: v1
@@ -94,16 +84,14 @@ kind: Pod
 metadata:
   name: transformer-echo
   annotations:
-    # Values `communication_type` can take are ["hpull://", "hrev://", "hpush://", "io://"].
-    # Visit https://github.com/NVIDIA/aistore/blob/master/docs/etl.md#communication-mechanisms 
-    # for more details.
+    # Values it can take ["hpull://","hrev://","hpush://"]
     communication_type: "{communication_type}://"
     wait_timeout: 5m
 spec:
   containers:
     - name: server
       image: aistorage/transformer_echo:latest
-      imagePullPolicy: IfNotPresent
+      imagePullPolicy: Always
       ports:
         - name: default
           containerPort: 80
@@ -114,9 +102,7 @@ spec:
           port: default
 """
 
-# Returns the transformed TensorFlow compatible data for the input tar files.
-# For more information on command options, visit
-# https://github.com/NVIDIA/ais-etl/blob/master/transformers/tar2tf/README.md.
+# returns the transformed TensorFlow compatible data for the input tar files
 # pylint: disable=unused-variable
 TAR2TF = """
 apiVersion: v1
@@ -124,81 +110,19 @@ kind: Pod
 metadata:
   name: tar2tf
   annotations:
-    # Values `communication_type` can take are ["hpull://", "hrev://", "hpush://", "io://"].
-    # Visit https://github.com/NVIDIA/aistore/blob/master/docs/etl.md#communication-mechanisms 
-    # for more details.
+    # Values it can take ["hpull://","hrev://","hpush://"]
     communication_type: "{communication_type}://"
     wait_timeout: 5m
 spec:
   containers:
     - name: server
       image: aistorage/transformer_tar2tf:latest
-      imagePullPolicy: IfNotPresent
+      imagePullPolicy: Always
       ports:
         - name: default
           containerPort: 80
       # To enable conversion e.g.
-      command: ['./tar2tf', '-l', '0.0.0.0', '-p', '80', '{arg}', '{val}']
-      readinessProbe:
-        httpGet:
-          path: /health
-          port: default
-"""
-
-# Returns the compressed/decompressed file.
-# For more information on command options, visit
-# https://github.com/NVIDIA/ais-etl/blob/master/transformers/compress/README.md.
-# pylint: disable=unused-variable
-COMPRESS = """
-apiVersion: v1
-kind: Pod
-metadata:
-  name: transformer-compress
-  annotations:
-    # Values `communication_type` can take are ["hpull://", "hrev://", "hpush://", "io://"].
-    # Visit https://github.com/NVIDIA/aistore/blob/master/docs/etl.md#communication-mechanisms 
-    # for more details.
-    communication_type: "{communication_type}://"
-    wait_timeout: 5m
-spec:
-  containers:
-    - name: server
-      image: aistorage/transformer_compress:latest
-      imagePullPolicy: IfNotPresent
-      ports:
-        - name: default
-          containerPort: 80
-      command: ['/code/server.py', '--listen', '0.0.0.0', '--port', '80', '{arg1}', '{val1}', '{arg2}', '{val2}']
-      readinessProbe:
-        httpGet:
-          path: /health
-          port: default
-"""
-
-# Returns the decoded audio file.
-# For more information on command options, visit
-# https://github.com/NVIDIA/ais-etl/blob/master/transformers/ffmpeg/README.md.
-# pylint: disable=unused-variable
-FFMPEG = """
-apiVersion: v1
-kind: Pod
-metadata:
-  name: transformer-ffmpeg
-  annotations:
-    # Values `communication_type` can take are ["hpull://", "hrev://", "hpush://", "io://"].
-    # Visit https://github.com/NVIDIA/aistore/blob/master/docs/etl.md#communication-mechanisms 
-    # for more details.
-    communication_type: "{communication_type}://"
-    wait_timeout: 5m
-spec:
-  containers:
-    - name: server
-      image: aistorage/transformer_ffmpeg:latest
-      imagePullPolicy: IfNotPresent
-      ports:
-        - name: default
-          containerPort: 80
-      command: ['/code/server.py', '--listen', '0.0.0.0', '--port', '80', '{arg1}', '{val1}', '{arg2}', '{val2}', '{arg3}', '{val3}', '{arg4}', '{val4}', '{arg5}', '{val5}']
+      command: ['./tar2tf', '-l', '0.0.0.0', '-p', '80', '{key}', '{value}']
       readinessProbe:
         httpGet:
           path: /health
