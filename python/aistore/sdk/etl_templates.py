e1958544e15ce7fd016a8b2e4514be7333443cae
Ryan Koo <rbk65@cornell.edu>
2023-07-28 18:26:18 -0700

    sdk/python: etl templates fix
    Signed-off-by: Ryan Koo <rbk65@cornell.edu>

--- python/aistore/sdk/etl_templates.py
+++ python/aistore/sdk/etl_templates.py
@@ -1,4 +1,4 @@
-# returns the md5 sum of the original data as the response
+# Returns the MD5 sum of the original data as the response.
 # pylint: disable=unused-variable
 MD5 = """
 apiVersion: v1
@@ -24,7 +24,7 @@ spec:
           port: default
 """
 
-# returns "Hello World!" on any request.
+# Returns "Hello World!" on any request.
 # pylint: disable=unused-variable
 HELLO_WORLD = """
 apiVersion: v1
@@ -50,7 +50,7 @@ spec:
           port: default
 """
 
-# returns the original data, with an md5 sum in the response headers.
+# Returns the original data, with an MD5 sum in the response headers.
 # pylint: disable=unused-variable
 GO_ECHO = """
 apiVersion: v1
@@ -76,7 +76,7 @@ spec:
           port: default
 """
 
-# returns the original data, with an md5 sum in the response headers
+# Returns the original data, with an MD5 sum in the response headers.
 # pylint: disable=unused-variable
 ECHO = """
 apiVersion: v1
@@ -102,7 +102,9 @@ spec:
           port: default
 """
 
-# returns the transformed TensorFlow compatible data for the input tar files
+# Returns the transformed TensorFlow compatible data for the input TAR files. For
+# more information on command options, visit
+# https://github.com/NVIDIA/ais-etl/blob/master/transformers/tar2tf/README.md.
 # pylint: disable=unused-variable
 TAR2TF = """
 apiVersion: v1
@@ -122,13 +124,43 @@ spec:
         - name: default
           containerPort: 80
       # To enable conversion e.g.
-      command: ['./tar2tf', '-l', '0.0.0.0', '-p', '80', '{key}', '{value}']
+      command: ['./tar2tf', '-l', '0.0.0.0', '-p', '80', '{arg}', '{val}']
       readinessProbe:
         httpGet:
           path: /health
           port: default
 """
 
+# Returns the compressed/decompressed file. For more information on command options, visit
+# https://github.com/NVIDIA/ais-etl/blob/master/transformers/compress/README.md.
+# pylint: disable=unused-variable
+COMPRESS = """
+apiVersion: v1
+kind: Pod
+metadata:
+  name: transformer-compress
+  annotations:
+    # Values `communication_type` can take are ["hpull://", "hrev://", "hpush://", "io://"].
+    # Visit https://github.com/NVIDIA/aistore/blob/master/docs/etl.md#communication-mechanisms 
+    # for more details.
+    communication_type: "{communication_type}://"
+    wait_timeout: 5m
+spec:
+  containers:
+    - name: server
+      image: aistorage/transformer_compress:latest
+      imagePullPolicy: Always
+      ports:
+        - name: default
+          containerPort: 80
+      command: ['/code/server.py', '--listen', '0.0.0.0', '--port', '80', '{arg1}', '{val1}', '{arg2}', '{val2}']
+      readinessProbe:
+        httpGet:
+          path: /health
+          port: default
+"""
+
+# pylint: disable=unused-variable
 KERAS_TRANSFORMER = """
 apiVersion: v1
 kind: Pod
