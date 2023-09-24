dcbad2fb5d88721501280cff3aee3e516d2477f5
Alex Aizman <alex.aizman@gmail.com>
2023-09-24 15:02:14 -0400

    micro-bench: compare xxash perf with respect to 32b alignment
    Signed-off-by: Alex Aizman <alex.aizman@gmail.com>

--- bench/microbenchmarks/hashspeed/hash_test.go
+++ bench/microbenchmarks/hashspeed/hash_test.go
@@ -1,6 +1,6 @@
 // Package hashspeed is a benchmark througput benchmark
 /*
- * Copyright (c) 2018-2021, NVIDIA CORPORATION. All rights reserved.
+ * Copyright (c) 2018-2023, NVIDIA CORPORATION. All rights reserved.
  */
 package hashspeed
 
@@ -10,6 +10,7 @@ import (
 	"runtime"
 	"sync/atomic"
 	"testing"
+	"unsafe"
 
 	"github.com/OneOfOne/xxhash"
 	"github.com/minio/highwayhash"
@@ -20,19 +21,80 @@ import (
 // go test -bench=.
 // go test -bench=Through -benchtime=10s
 // go test -bench=Through -benchtime=30s -benchmem
+//
+// go test -v -tags=debug -bench=ID -benchtime=10s
 
-const maxSize = 64 * 1024 * 1024
+const (
+	numThr = 64 * 1024 * 1024
+	numIDs = 262144
+
+	sizeID = 16
+)
+
+const MLCG32 = 1103515245 // xxhash seed
 
 var (
 	cores = runtime.GOMAXPROCS(0)
 	hwkey [32]byte
 	vec   = make([][]byte, cores)
+
+	vids       = make([][]byte, numIDs)
+	vecAligned = make([][]int32, numIDs)
 )
+
+// 6 1 &[88 158 198 47 235 152 81 107 49 248 216 192 90 239 228 121]
+// 6 2 [2fc69e58 6b5198eb -3f2707cf 79e4ef5a]
+// 6 3 [88 158 198 47 235 152 81 107 49 248 216 192 90 239 228 121]
+
+func BenchmarkID(b *testing.B) {
+	rnd := rand.New(rand.NewSource(0xa5a5a5a5a5a5))
+	for i := range vids {
+		vids[i] = make([]byte, sizeID+1) // NOTE: to force misalign
+		rnd.Read(vids[i])
+
+		vecAligned[i] = make([]int32, 4)
+		bytes := (*[sizeID]byte)(unsafe.Pointer(&vecAligned[i][0]))
+		copy(bytes[:], vids[i])
+	}
+
+	b.Run("aligned", func(b *testing.B) {
+		aligned(b)
+	})
+
+	b.Run("na", func(b *testing.B) {
+		na(b)
+	})
+}
+
+func aligned(b *testing.B) {
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			for i := 0; i < numIDs; i++ {
+				v := vecAligned[i]
+				bytes := (*[sizeID]byte)(unsafe.Pointer(&v[0]))
+				_ = xxhash.Checksum64S(bytes[:], MLCG32)
+			}
+		}
+	})
+}
+
+func na(b *testing.B) {
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			for i := 0; i < numIDs; i++ {
+				v := vids[i][1:] // force misalignment
+				_ = xxhash.Checksum64S(v, MLCG32)
+			}
+		}
+	})
+}
 
 func BenchmarkThroughput(b *testing.B) {
 	rnd := rand.New(rand.NewSource(0xa5a5a5a5a5a5))
 	for i := range vec {
-		vec[i] = make([]byte, maxSize)
+		vec[i] = make([]byte, numThr)
 		rnd.Read(vec[i])
 	}
 	tests := []struct {
