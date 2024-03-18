9134c006b84382bd305d99c76a18121449e050ae
Alex Aizman <alex.aizman@gmail.com>
2024-03-18 12:20:46 -0400

    follow-up
    Signed-off-by: Alex Aizman <alex.aizman@gmail.com>

--- ios/dutils_darwin.go
+++ ios/dutils_darwin.go
@@ -1,7 +1,7 @@
 // Package ios is a collection of interfaces to the local storage subsystem;
 // the package includes OS-dependent implementations for those interfaces.
 /*
- * Copyright (c) 2018-2023, NVIDIA CORPORATION. All rights reserved.
+ * Copyright (c) 2018-2024, NVIDIA CORPORATION. All rights reserved.
  */
 package ios
 
@@ -15,7 +15,7 @@ type LsBlk struct{}
 
 func lsblk(string, bool) *LsBlk { return nil }
 
-func fs2disks(*LsBlk, string, bool) (disks FsDisks) {
+func fs2disks(*LsBlk, string, string, int, bool) (disks FsDisks) {
 	driveStats, err := iostat.ReadDriveStats()
 	if err != nil || len(driveStats) == 0 {
 		return
