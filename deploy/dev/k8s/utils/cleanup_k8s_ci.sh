888da5de8f2e75c2b17819f3d45e90771aae2578
Aaron Wilson <aawilson@nvidia.com>
2024-06-14 15:29:51 -0500

    tests: Cleanup logs in minikube VM after running k8s tests
    Signed-off-by: Aaron Wilson <aawilson@nvidia.com>

--- deploy/dev/k8s/utils/cleanup_k8s_ci.sh
+++ deploy/dev/k8s/utils/cleanup_k8s_ci.sh
@@ -2,3 +2,10 @@
 kubectl delete pods -l app=ais,type=ais-target || true
 kubectl delete pods -l app=ais,type=ais-proxy || true
 kubectl delete svc -l app=ais || true
+
+# Use a cleanup job to delete any AIS files mounted with hostpath inside the minikube vm
+export PARENT_DIR="/tmp"
+export HOST_PATH="/tmp/ais"
+export JOB_NAME="test-cleanup"
+envsubst < kube_templates/cleanup_job_template.yml > cleanup_job.yml
+kubectl apply -f cleanup_job.yml
