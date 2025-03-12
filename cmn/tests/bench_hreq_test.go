e8f8c042520b63e3e463c7f7db44e78b58e7f4c6
Alex Aizman <alex.aizman@gmail.com>
2025-03-12 10:01:44 -0400

    tests: update URL parsing bench
    * related commit: 70262010873d
    
    Signed-off-by: Alex Aizman <alex.aizman@gmail.com>

--- cmn/tests/bench_hreq_test.go
+++ cmn/tests/bench_hreq_test.go
@@ -1,5 +1,4 @@
-// Package cmn provides common constants, types, and utilities for AIS clients
-// and AIStore.
+// Package test provides tests for common low-level types and utilities for all aistore projects
 /*
  * Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
  */
@@ -7,22 +6,44 @@ package tests_test
 
 import (
 	"net/http"
+	"net/url"
+	"path"
 	"testing"
 
+	"github.com/NVIDIA/aistore/api/apc"
 	"github.com/NVIDIA/aistore/cmn"
+	"github.com/NVIDIA/aistore/cmn/cos"
 )
 
-// go test -bench=. -benchmem -benchtime=5s
+// go test -bench=Hpool -benchmem -benchtime=10s -run=Hpool
 
-func BenchmarkWithoutHpool(b *testing.B) {
+var objs [16384]string
+
+func _args(i int) *cmn.HreqArgs {
+	if i >= len(objs) {
+		i = 0
+	}
+	return &cmn.HreqArgs{
+		Method: http.MethodGet,
+		Base:   "http://www.aistore.com",
+		Path:   path.Join(apc.URLPathObjects.S, "bucket", objs[i]),
+		Query:  url.Values{"a": []string{"b"}, "provider": []string{"ais"}},
+	}
+}
+
+func init() {
+	for i := range objs {
+		objs[i] = path.Join(cos.CryptoRandS(8), cos.CryptoRandS(8))
+	}
+}
+
+func BenchmarkWitHpool(b *testing.B) {
+	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
+		var i int
 		for pb.Next() {
-			args := &cmn.HreqArgs{
-				Method: http.MethodGet,
-				Base:   "http://localhost:80",
-				Path:   "/v1/bucket",
-			}
-			_, err := args.ReqDeprecated()
+			i++
+			_, err := _args(i).Req()
 			if err != nil {
 				panic(err)
 			}
@@ -30,15 +51,13 @@ func BenchmarkWithoutHpool(b *testing.B) {
 	})
 }
 
-func BenchmarkWitHpool(b *testing.B) {
+func BenchmarkWithoutHpool(b *testing.B) {
+	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
+		var i int
 		for pb.Next() {
-			args := &cmn.HreqArgs{
-				Method: http.MethodGet,
-				Base:   "http://localhost:80",
-				Path:   "/v1/bucket",
-			}
-			_, err := args.Req()
+			i++
+			_, err := _args(i).ReqDeprecated()
 			if err != nil {
 				panic(err)
 			}
