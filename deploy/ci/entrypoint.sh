8a56677aa9f9a8019ec9ebca44cf4233f16bc990
Ryan Koo <rbk65@cornell.edu>
2025-07-02 11:46:21 -0700

    gitlab-ci: lower vm.swappiness (mem pressure)
    Signed-off-by: Ryan Koo <rbk65@cornell.edu>

--- deploy/ci/entrypoint.sh
+++ deploy/ci/entrypoint.sh
@@ -4,4 +4,7 @@ set -e
 sysctl -w net.ipv6.conf.all.disable_ipv6=0
 sysctl -w net.ipv6.conf.default.disable_ipv6=0
 
+# For AIStore memory management and pressure detection (see docs/performance.md)
+sysctl -w vm.swappiness=10
+
 exec "$@"
