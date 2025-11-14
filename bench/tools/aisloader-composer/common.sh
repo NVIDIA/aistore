5cebc7489142967e3629cc4a505a938d00debf52
Abhishek Gaikwad <gaikwadabhishek1997@gmail.com>
2025-11-14 15:08:09 -0800

    aisloader-composer: add ansible configuration for perf optimizations
    - Introduced a new ansible.cfg file with performance optimizations and settings.
    - Removed the export of ANSIBLE_DISPLAY_SKIPPED_HOSTS (part of config).
    
    Signed-off-by: Abhishek Gaikwad <gaikwadabhishek1997@gmail.com>

--- bench/tools/aisloader-composer/common.sh
+++ bench/tools/aisloader-composer/common.sh
@@ -3,12 +3,9 @@
 INVENTORY="inventory/oci.yaml"
 GRAFANA_HOST="aismonitor"
 
-# Hide output for tasks we aren't executing
-export ANSIBLE_DISPLAY_SKIPPED_HOSTS=false
-
 run_ansible_playbook() {
     local playbook="$1"
     # Any extra variables to pass to the playbook
     local env="$2"
-    ansible-playbook -i "$INVENTORY" "$playbook" -f 10 --become -e "ansible_become_pass=y grafana_host=$GRAFANA_HOST $2"
+    ansible-playbook -i "$INVENTORY" "$playbook" --become -e "grafana_host=$GRAFANA_HOST $2"
 }
