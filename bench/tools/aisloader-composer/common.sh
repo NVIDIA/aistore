ff475b77b50ca58e3bf844a620da5d1ea96e3658
Abhishek Gaikwad <gaikwadabhishek1997@gmail.com>
2025-11-21 16:23:37 -0800

    aisloader-composer: follow-up
    - Added 'playbooks/files/' to .gitignore to exclude crt and token.
    - Updated ansible.cfg to set interpreter_python to auto_silent for python version issues.
    - Refactored common.sh to conditionally append extra variables for ansible-playbook execution.
    - Modified aisloader_setup.yaml to change file permissions from 'a+rwx' to 'a+x'.
    - Updated perms for copy_authn_token.yaml and copy_crt.yaml.
    
    Signed-off-by: Abhishek Gaikwad <gaikwadabhishek1997@gmail.com>

--- bench/tools/aisloader-composer/common.sh
+++ bench/tools/aisloader-composer/common.sh
@@ -5,7 +5,8 @@ GRAFANA_HOST="aismonitor"
 
 run_ansible_playbook() {
     local playbook="$1"
-    # Any extra variables to pass to the playbook
-    local env="$2"
-    ansible-playbook -i "$INVENTORY" "$playbook" --become -e "grafana_host=$GRAFANA_HOST $2"
+    local extra_vars="${2:+ $2}"  # Prepend space only if $2 exists
+    
+    ansible-playbook -i "$INVENTORY" "$playbook" --become \
+        -e "grafana_host=$GRAFANA_HOST$extra_vars"
 }
