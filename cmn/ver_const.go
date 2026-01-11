62b89e90a742ae02282ad1524d1f7bfab565b60e
Alex Aizman <alex.aizman@gmail.com>
2026-01-11 14:46:10 -0500

    bump rc2
    * 4.2.rc2
    
    Signed-off-by: Alex Aizman <alex.aizman@gmail.com>

--- cmn/ver_const.go
+++ cmn/ver_const.go
@@ -26,7 +26,7 @@ const GitHubHome = "https://github.com/NVIDIA/aistore"
 //   `jsp` formats its *signature* and other implementation details.
 
 const (
-	VersionAIStore = "4.2.rc1"
+	VersionAIStore = "4.2.rc2"
 	VersionCLI     = "1.23"
 	VersionLoader  = "2.1"
 	VersionAuthN   = "1.5"
