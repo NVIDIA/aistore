619543fb65c5be097fa577ef1eb2692a615d4019
Alex Aizman <alex.aizman@gmail.com>
2026-05-27 18:32:47 -0400

    docs: relnotes 4.6 (part three)
    Signed-off-by: Alex Aizman <alex.aizman@gmail.com>

--- cmn/ver_const.go
+++ cmn/ver_const.go
@@ -26,9 +26,9 @@ const GitHubHome = "https://github.com/NVIDIA/aistore"
 //   `jsp` formats its *signature* and other implementation details.
 
 const (
-	VersionAIStore = "4.5"
-	VersionCLI     = "1.28"
-	VersionLoader  = "2.22"
+	VersionAIStore = "4.6"
+	VersionCLI     = "1.29"
+	VersionLoader  = "2.23"
 	VersionAuthN   = "2.1"
 )
 
